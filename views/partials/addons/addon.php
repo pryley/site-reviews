6e05e5f074104089681da253eb30ead5b48eb201
Paul Ryley <paul@geminilabs.io>
2018-06-02 20:14:38 +0400

    fix addons page

--- views/partials/addons/addon.php
+++ views/partials/addons/addon.php
@@ -2,13 +2,17 @@
 
 <div class="glsr-addon-wrap">
 	<div class="glsr-addon">
-		<a href="{{ link }}" class="glsr-addon-screenshot" data-name="{{ name }}">{{ name }}</a>
+		<a href="{{ link }}" class="glsr-addon-screenshot" data-name="{{ name }}">
+			<span class="screen-reader-text">{{ name }}</span>
+		</a>
 		<div class="glsr-addon-description">
+			<h3 class="glsr-addon-name">{{ title }}</h3>
 			<p>{{ description }}</p>
 		</div>
-		<h3 class="glsr-addon-name">{{ title }}</h3>
-		<a href="{{ link }}" class="glsr-addon-link button button-secondary">
-			<?= __( 'More Info', 'site-reviews' ); ?>
-		</a>
+		<div class="glsr-addon-footer">
+			<a href="{{ link }}" class="glsr-addon-link button button-secondary">
+				<?= __( 'More Info', 'site-reviews' ); ?>
+			</a>
+		</div>
 	</div>
 </div>
