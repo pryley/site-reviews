d13e13709149c31eed166f22fa67796042c59e9e
Paul Ryley <paul@geminilabs.io>
2018-06-03 02:07:07 +0400

    __()

--- views/pages/settings/schema.php
+++ views/pages/settings/schema.php
@@ -1,7 +1,6 @@
 <?php defined( 'WPINC' ) || die; ?>
 
-<p>The JSON-LD schema appears in Google's search results and shows the star rating and other information about your reviews.</p>
-
+<p><?= __( 'The JSON-LD schema appears in Google\'s search results and shows the star rating and other information about your reviews.', 'site-reviews' ); ?></p>
 <table class="form-table">
 	<tbody>
 		{{ rows }}
--- views/partials/editor/metabox-response.php
+++ views/partials/editor/metabox-response.php
@@ -2,4 +2,4 @@
 
 <label class="screen-reader-text" for="response"><?= __( 'Respond Publicly', 'site-reviews' ); ?></label>
 <textarea class="glsr-response" name="response" id="response" rows="1" cols="40"><?= $response; ?></textarea>
-<p>If you need to publicly respond to this review, enter your response here.</p>
+<p><?= __( 'If you need to publicly respond to this review, enter your response here.', 'site-reviews' ); ?></p>
