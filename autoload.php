cd46957f1553e3d91656318ad668c1c88b488717
Paul Ryley <paul@geminilabs.io>
2018-06-13 14:50:06 +0400

    edit autoload codestyle

--- autoload.php
+++ autoload.php
@@ -6,17 +6,17 @@ require_once( ABSPATH.WPINC.'/class-phpass.php' );
 
 spl_autoload_register( function( $className ) {
 	$namespaces = [
-		'GeminiLabs\\SiteReviews\\'        => __DIR__.'/plugin/',
+		'GeminiLabs\\SiteReviews\\' => __DIR__.'/plugin/',
 		'GeminiLabs\\SiteReviews\\Tests\\' => __DIR__.'/tests/',
-		'GeminiLabs\\SchemaOrg\\'          => __DIR__.'/vendor/geminilabs/schema-org/src/',
-		'Sepia\\PoParser\\'                => __DIR__.'/vendor/qcubed/i18n/src/Sepia/',
-		'Sinergi\\BrowserDetector\\'       => __DIR__.'/vendor/sinergi/browser-detector/src/',
-		'Vectorface\\Whip\\'               => __DIR__.'/vendor/vectorface/whip/src/',
+		'GeminiLabs\\SchemaOrg\\' => __DIR__.'/vendor/geminilabs/schema-org/src/',
+		'Sepia\\PoParser\\' => __DIR__.'/vendor/qcubed/i18n/src/Sepia/',
+		'Sinergi\\BrowserDetector\\' => __DIR__.'/vendor/sinergi/browser-detector/src/',
+		'Vectorface\\Whip\\' => __DIR__.'/vendor/vectorface/whip/src/',
 	];
-	foreach( $namespaces as $prefix => $base_dir ) {
+	foreach( $namespaces as $prefix => $baseDir ) {
 		$len = strlen( $prefix );
 		if( strncmp( $prefix, $className, $len ) !== 0 )continue;
-		$file = $base_dir.str_replace( '\\', '/', substr( $className, $len )).'.php';
+		$file = $baseDir.str_replace( '\\', '/', substr( $className, $len )).'.php';
 		if( !file_exists( $file ))continue;
 		require $file;
 		break;
