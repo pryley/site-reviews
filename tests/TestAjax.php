e0ac9db3a653623334334906eb0692b67541a32d
Paul Ryley <paul@geminilabs.io>
2018-06-27 16:17:44 -0600

    fix ajax tests

--- tests/TestAjax.php
+++ tests/TestAjax.php
@@ -26,7 +26,7 @@ class TestAjax extends WP_Ajax_UnitTestCase
 	protected function ajax_response( $request )
 	{
 		$_POST['ajax_request'] = true;
-		$_POST[Application::ID] = $request;
+		$_POST['request'] = $request;
 		try {
 			$this->_handleAjax( Application::PREFIX.'action' );
 		}
