a6e3077520ead7f47307eeeeb240e53185fef406
Paul Ryley <paul@geminilabs.io>
2018-07-30 20:40:05 -0600

    validation image for stars

--- config/inline-styles.php
+++ config/inline-styles.php
@@ -2,6 +2,7 @@
 
 return [
 	':star-empty' => glsr()->url( 'assets/images/star-empty.svg' ),
+	':star-error' => glsr()->url( 'assets/images/star-error.svg' ),
 	':star-full' => glsr()->url( 'assets/images/star-full.svg' ),
 	':star-half' => glsr()->url( 'assets/images/star-half.svg' ),
 ];
