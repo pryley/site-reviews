0ad9d6681ac524464a3895e22bce738a71a0d434
Paul Ryley <paul@geminilabs.io>
2018-09-13 13:04:02 +0100

    add additional compatibility hooks

--- compatibility.php
+++ compatibility.php
@@ -3,6 +3,15 @@
 defined( 'WPINC' ) || die;
 
 /**
+ * @param \GeminiLabs\SiteReviews\Modules\Html\Builder $instance
+ * @return void
+ */
+add_action( 'site-reviews/customize/divi', function( $instance ) {
+	if( $instance->tag != 'label' || $instance->args['type'] != 'checkbox' )return;
+	$instance->args['text'] = '<i></i>'.$instance->args['text'];
+});
+
+/**
  * @param array $scriptHandles
  * @return array
  * @see https://wordpress.org/plugins/speed-booster-pack/
@@ -13,10 +22,20 @@ add_filter( 'sbp_exclude_defer_scripts', function( $scriptHandles ) {
 });
 
 /**
- * @param \GeminiLabs\SiteReviews\Modules\Html\Builder $instance
- * @return void
+ * Fix to display all reviews when sorting by rank
+ * @param array $query
+ * @return array
+ * @see ??
  */
-add_action( 'site-reviews/customize/divi', function( $instance ) {
-	if( $instance->tag != 'label' || $instance->args['type'] != 'checkbox' )return;
-	$instance->args['text'] = '<i></i>'.$instance->args['text'];
-});
+add_filter( 'sf_edit_query_args', function( $query ) {
+	if( !empty( $query['meta_key'] ) && $query['meta_key'] == '_glsr_ranking' ) {
+		unset( $query['meta_key'] );
+		$query['meta_query'] = [
+			'relation' => 'OR',
+			['key' => '_glsr_ranking', 'compare' => 'EXISTS'],
+			['key' => '_glsr_ranking', 'compare' => 'NOT EXISTS'],
+		];
+	}
+	return $query;
+}, 20 );
+
