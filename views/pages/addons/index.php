6c7ae84e333b9774422376119e738e1f1b9424de
Paul Ryley <paul@geminilabs.io>
2018-12-20 12:12:23 -0700

    add an add-on notice

--- views/pages/addons/index.php
+++ views/pages/addons/index.php
@@ -2,12 +2,16 @@
 
 <div class="wrap">
 	<h1 class="page-title"><?= esc_html( get_admin_page_title() ); ?></h1>
-	<p><?= __( 'The following Add-Ons extend the functionality of Site Reviews.', 'site-reviews' ); ?></p>
+	<div class="notice notice-warning is-dismissible">
+		<p><?= __( 'You may be wondering when these add-ons will be released as they have been "Coming soon" for quite some time. Well, now that Site Reviews v3.0 (a major overhaul of the plugin) has been released, the only thing delaying them is the distribution platform which is currently being built. Expect to see them in the coming months!', 'site-reviews' ); ?></p>
+		<p><?= __( 'Thank you for your patience.', 'site-reviews' ); ?></p>
+	</div>
+	<p><?= __( 'Add-Ons extend the functionality of Site Reviews.', 'site-reviews' ); ?></p>
 	<div class="glsr-addons wp-clearfix">
 	<?php
 		$template->render( 'partials/addons/addon', [
 			'context' => [
-				'description' => __( 'Add images to your review submissions.', 'site-reviews' ),
+				'description' => __( 'Allow your visitors to submit multiple images with their reviews.', 'site-reviews' ),
 				'link' => 'https://niftyplugins.com/addons/site-reviews-images/',
 				'slug' => 'images',
 				'title' => 'Images',
@@ -16,7 +20,7 @@
 		]);
 		$template->render( 'partials/addons/addon', [
 			'context' => [
-				'description' => __( 'Sync your Tripadvisor reviews and display them on your site.', 'site-reviews' ),
+				'description' => __( 'Sync your Tripadvisor reviews to your website and manage them with Site Reviews.', 'site-reviews' ),
 				'link' => 'https://niftyplugins.com/addons/site-reviews-tripadvisor/',
 				'slug' => 'tripadvisor',
 				'title' => 'Tripadvisor Reviews',
