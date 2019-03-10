939a309ed36fd8bf45c98065107d35640afac85b
Paul Ryley <paul@geminilabs.io>
2019-03-10 15:26:43 +0100

    let the ReviewController handle all count modifications

--- plugin/Controllers/EditorController/Metaboxes.php
+++ plugin/Controllers/EditorController/Metaboxes.php
@@ -2,8 +2,6 @@
 
 namespace GeminiLabs\SiteReviews\Controllers\EditorController;
 
-use GeminiLabs\SiteReviews\Database\CountsManager;
-use GeminiLabs\SiteReviews\Database\ReviewManager;
 use GeminiLabs\SiteReviews\Helper;
 
 class Metaboxes
@@ -16,10 +14,6 @@ class Metaboxes
 	{
 		if( !wp_verify_nonce( glsr( Helper::class )->filterInput( '_nonce-assigned-to' ), 'assigned_to' ))return;
 		$assignedTo = strval( glsr( Helper::class )->filterInput( 'assigned_to' ));
-		if( get_post_meta( $postId, 'assigned_to', true ) != $assignedTo ) {
-			$review = glsr( ReviewManager::class )->single( get_post( $postId ));
-			glsr( CountsManager::class )->decreasePostCounts( $review );
-		}
 		update_post_meta( $postId, 'assigned_to', $assignedTo );
 	}
 
