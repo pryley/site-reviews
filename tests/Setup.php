cc9e05774e3247d9bc7800438efd65ab7f7593fd
Paul Ryley <paul@geminilabs.io>
2019-08-06 22:54:14 +0200

    use Faker in tests

--- tests/Setup.php
+++ tests/Setup.php
@@ -2,33 +2,35 @@
 
 namespace GeminiLabs\SiteReviews\Tests;
 
+use Faker\Factory;
 use GeminiLabs\SiteReviews\Database\DefaultsManager;
 use GeminiLabs\SiteReviews\Database\OptionManager;
 
 trait Setup
 {
-	public function setUp()
-	{
-		parent::setUp();
-		$_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
-		$_SERVER['SERVER_NAME'] = '';
-		$PHP_SELF = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
-		glsr()->activate();
+    public function setUp()
+    {
+        parent::setUp();
+        $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
+        $_SERVER['SERVER_NAME'] = '';
+        $PHP_SELF = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
+        glsr()->activate();
         glsr(OptionManager::class)->set(glsr(DefaultsManager::class)->get());
-		$this->review = [
-			'_action' => 'submit-review',
+        $faker = Factory::create();
+        $this->review = [
+            '_action' => 'submit-review',
             '_nonce' => wp_create_nonce('submit-review'),
-			'_post_id' => '13',
-			'_referer' => $PHP_SELF,
-			'content' => 'abcdefg',
-			'email' => 'jane@doe.com',
+            '_post_id' => '13',
+            '_referer' => $PHP_SELF,
+            'content' => $faker->text,
+            'email' => $faker->email,
             'excluded' => '[]',
-			'form_id' => 'abcdef',
-			'name' => 'Jane Doe',
-			'rating' => '5',
-			'terms' => '1',
-			'title' => 'Test Review',
-		];
-		// save initial plugin settings here if needed
-	}
+            'form_id' => $faker->slug,
+            'name' => $faker->name,
+            'rating' => '5',
+            'terms' => '1',
+            'title' => $faker->sentence,
+        ];
+        // save initial plugin settings here if needed
+    }
 }
