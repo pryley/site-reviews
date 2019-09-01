50b46fe9a3653dad9afa7efe79cec0cacff3f1c6
Paul Ryley <paul@geminilabs.io>
2019-09-01 18:19:29 +0100

    bump version

--- site-reviews.php
+++ site-reviews.php
@@ -7,7 +7,7 @@
  * Plugin Name: Site Reviews
  * Plugin URI:  https://wordpress.org/plugins/site-reviews
  * Description: Receive and display reviews on your website
- * Version:     3.5.4
+ * Version:     4.0.0
  * Author:      Paul Ryley
  * Author URI:  https://niftyplugins.com
  * License:     GPL2
