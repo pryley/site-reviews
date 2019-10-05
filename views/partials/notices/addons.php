65906070e0aadbee3ee0c39f8c4b292ca199c6d8
Paul Ryley <paul@geminilabs.io>
2019-10-05 17:59:38 +0100

    update addons notice text

--- views/partials/notices/addons.php
+++ views/partials/notices/addons.php
@@ -1,4 +1,3 @@
 <div class="notice notice-warning is-dismissible">
-    <p><?= __('Now that Site Reviews v3.0 (a major overhaul of the plugin) has been released, the only thing delaying the add-ons is the distribution platform which is currently being built. Expect to see them in the coming months!', 'site-reviews'); ?></p>
-    <p><?= __('Thank you for your patience.', 'site-reviews'); ?></p>
+    <p><?= __('Expect to see these add-ons released in the coming months! Thank you again for your continued patience.', 'site-reviews'); ?></p>
 </div>
