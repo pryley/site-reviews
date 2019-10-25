2bd026ef70d89f08fa341eb7ed12ed16b9b7955d
Paul Ryley <paul@geminilabs.io>
2019-10-25 20:01:02 +0200

    fix Console

--- plugin/Modules/Console.php
+++ plugin/Modules/Console.php
@@ -319,6 +319,10 @@ class Console
     protected function getBacktraceLine()
     {
         $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 6);
+        $search = array_search('glsr_log', glsr_array_column($backtrace, 'function'));
+        if (false !== $search) {
+            return $this->buildBacktraceLine($backtrace, $search);
+        }
         $search = array_search('log', glsr_array_column($backtrace, 'function'));
         if (false !== $search) {
             $index = '{closure}' == Arr::get($backtrace, ($search + 2).'.function')
