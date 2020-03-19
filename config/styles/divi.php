f9fba94ed9d91bf0483ba3ccc365fd01a2f1aa80
Paul Ryley <paul@geminilabs.io>
2020-03-19 12:04:06 -0700

    fix divi style

--- config/styles/divi.php
+++ config/styles/divi.php
@@ -2,6 +2,15 @@
 
 return [
     'fields' => [
+        'input' => 'input',
+        'input_checkbox' => 'input',
+        'input_radio' => 'input',
         'label' => 'et_pb_contact_form_label',
+        'select' => 'et_pb_contact_select input',
+        'textarea' => 'et_pb_contact_message input',
+    ],
+    'validation' => [
+        'field_error_class' => 'et_contact_error',
+        'input_error_class' => 'et_contact_error',
     ],
 ];
