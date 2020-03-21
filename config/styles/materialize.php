5746102668e493ecc74fc273c3dc119c7170c472
Paul Ryley <paul@geminilabs.io>
2020-03-20 18:33:24 -0700

    fix materialize classes

--- config/styles/materialize.php
+++ config/styles/materialize.php
@@ -10,9 +10,8 @@ return [
     ],
     'validation' => [
         'error_tag' => 'span',
-        'input_error_class' => 'wpcf7-not-valid',
-        'error_tag_class' => 'wpcf7-not-valid-tip',
-        'input_error_class' => 'wpcf7-not-valid',
+        'error_tag_class' => 'red-text',
+        'input_error_class' => 'invalid',
         'message_tag_class' => 'wpcf7-response-output',
     ],
 ];
