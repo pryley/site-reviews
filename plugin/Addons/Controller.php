bae2fc5f4b82042b9a1fa9156ea52497d70bd530
Paul Ryley <paul@geminilabs.io>
2020-03-22 19:05:59 -0700

    check permission for displaying addon plugin link

--- plugin/Addons/Controller.php
+++ plugin/Addons/Controller.php
@@ -60,9 +60,11 @@ abstract class Controller extends BaseController
      */
     public function filterActionLinks(array $links)
     {
-        $links['settings'] = glsr(Builder::class)->a(_x('Settings', 'admin-text', 'site-reviews'), [
-            'href' => admin_url('edit.php?post_type='.glsr()->post_type.'&page=settings#tab-addons'),
-        ]);
+        if (glsr()->hasPermission('settings')) {
+            $links['settings'] = glsr(Builder::class)->a(_x('Settings', 'admin-text', 'site-reviews'), [
+                'href' => admin_url('edit.php?post_type='.glsr()->post_type.'&page=settings#tab-addons'),
+            ]);
+        }
         return $links;
     }
 
