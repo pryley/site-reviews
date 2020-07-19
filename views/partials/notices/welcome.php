218ca3a9523c61124fbc5ca2278f178681c1b4bc
Paul Ryley <paul@geminilabs.io>
2020-07-19 16:02:28 -0500

    fix welcome page link in notice

--- views/partials/notices/welcome.php
+++ views/partials/notices/welcome.php
@@ -1,4 +1,4 @@
 <div class="notice notice-info is-dismissible glsr-notice" data-dismiss="welcome">
     <p><?= $text; ?></p>
-    <p><a class="button button-small" href="<?= admin_url('edit.php?post_type='.glsr()->post_type.'&page=welcome#tab-whatsnew'); ?>"><?= _x('See what\'s new', 'admin-text', 'site-reviews'); ?></a></p>
+    <p><a class="button button-small" href="<?= admin_url('index.php?page='.glsr()->id.'-welcome#tab-whatsnew'); ?>"><?= _x('See what\'s new', 'admin-text', 'site-reviews'); ?></a></p>
 </div>
