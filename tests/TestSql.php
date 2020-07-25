9cc2ed43fca5fd5c071e43df77c42bd8f2b7da64
Paul Ryley <paul@geminilabs.io>
2020-07-25 18:53:28 -0500

    Sql tests

--- tests/TestSql.php
+++ tests/TestSql.php
@@ -10,13 +10,20 @@ class TestSql extends WP_Ajax_UnitTestCase
     public function test_sql_join()
     {
         $query = glsr(Query::class);
+        $postId = self::factory()->post->create();
+        $termId = self::factory()->term->create(['taxonomy' => glsr()->taxonomy]);
+        $userId = self::factory()->user->create();
         $query->setArgs([
-            'assigned_posts' => 1,
-            'assigned_terms' => 1,
-            'assigned_users' => 1,
+            'assigned_posts' => $postId,
+            'assigned_terms' => $termId,
+            'assigned_users' => $userId,
         ]);
-        $clauses = array_unique($query->clauses('join'));
-        $this->assertCount(3, $clauses);
+        $clauses = array_unique(array_filter($query->clauses('join')));
+        $this->assertCount(4, $clauses);
+        $this->assertTrue(in_array('INNER JOIN wptests_glsr_assigned_posts AS apt ON r.ID = apt.rating_id', $clauses));
+        $this->assertTrue(in_array('INNER JOIN wptests_glsr_assigned_terms AS att ON r.ID = att.rating_id', $clauses));
+        $this->assertTrue(in_array('INNER JOIN wptests_glsr_assigned_users AS aut ON r.ID = aut.rating_id', $clauses));
+        $this->assertTrue(in_array('INNER JOIN wptests_posts AS p ON r.review_id = p.ID', $clauses));
     }
 
     public function _test_sql_order_by()
