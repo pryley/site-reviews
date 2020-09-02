3b6782d8332325482cbe8628e568ccd2a79c340f
Paul Ryley <paul@geminilabs.io>
2020-09-02 13:04:48 -0500

    test for meta counts after assigning/unassigning

--- tests/ReviewManagerTest.php
+++ tests/ReviewManagerTest.php
@@ -30,19 +30,19 @@ class ReviewManagerTest extends WP_UnitTestCase
         $faker = Factory::create();
         $this->request = new Request([
             '_action' => 'submit-review',
+            '_ajax_request' => 1,
             '_post_id' => 1,
-            '_referer' => '',
+            '_referer' => $this->referer,
             'assigned_posts' => '',
             'assigned_terms' => '',
             'assigned_users' => '',
             'excluded' => '',
-            'form_id' => 'glsr_123',
+            'form_id' => $faker->slug,
             'rating' => 5,
             'title' => $faker->sentence,
             'content' => $faker->text,
             'name' => $faker->name,
             'email' => $faker->email,
-            '_ajax_request' => 1,
         ]);
         $this->termArgs = [
             'taxonomy' => glsr()->taxonomy,
@@ -51,56 +51,89 @@ class ReviewManagerTest extends WP_UnitTestCase
 
     public function test_assign_post()
     {
+        // automatically assign posts
         $posts = [
             self::factory()->post->create(),
             self::factory()->post->create(),
         ];
         $request = $this->request;
-        $request->set('assigned_posts', implode(',', $posts));
-        $review = $this->createReview($request);
+        $review = $this->createReview($request->merge([
+            'assigned_posts' => implode(',', $posts),
+        ]));
         $this->assertTrue($review->isValid());
         $this->assertEquals($review->assigned_posts, $posts);
+        foreach ($posts as $postId) {
+            $this->assertEquals(get_post_meta($postId, '_glsr_average', true), 5);
+            $this->assertTrue(get_post_meta($postId, '_glsr_ranking', true) > 0);
+            $this->assertEquals(get_post_meta($postId, '_glsr_reviews', true), 1);
+        }
+        // manually assign post
         $postId = self::factory()->post->create();
         $posts[] = $postId;
         glsr(ReviewManager::class)->assignPost($review, $postId);
         $review = glsr(ReviewManager::class)->get($review->ID);
         $this->assertEquals($review->assigned_posts, $posts);
+        $this->assertEquals(get_post_meta($postId, '_glsr_average', true), 5);
+        $this->assertTrue(get_post_meta($postId, '_glsr_ranking', true) > 0);
+        $this->assertEquals(get_post_meta($postId, '_glsr_reviews', true), 1);
     }
 
     public function test_assign_term()
     {
+        // automatically assign terms
         $terms = [
             self::factory()->term->create($this->termArgs),
             self::factory()->term->create($this->termArgs),
         ];
         $request = $this->request;
-        $request->set('assigned_terms', implode(',', $terms));
-        $review = $this->createReview($request);
+        $review = $this->createReview($request->merge([
+            'assigned_terms' => implode(',', $terms),
+        ]));
         $this->assertTrue($review->isValid());
         $this->assertEquals($review->assigned_terms, $terms);
+        foreach ($terms as $termId) {
+            $this->assertEquals(get_term_meta($termId, '_glsr_average', true), 5);
+            $this->assertTrue(get_term_meta($termId, '_glsr_ranking', true) > 0);
+            $this->assertEquals(get_term_meta($termId, '_glsr_reviews', true), 1);
+        }
+        // manually assign term
         $termId = self::factory()->term->create($this->termArgs);
         $terms[] = $termId;
         glsr(ReviewManager::class)->assignTerm($review, $termId);
         $review = glsr(ReviewManager::class)->get($review->ID);
         $this->assertEquals($review->assigned_terms, $terms);
+        $this->assertEquals(get_term_meta($termId, '_glsr_average', true), 5);
+        $this->assertTrue(get_term_meta($termId, '_glsr_ranking', true) > 0);
+        $this->assertEquals(get_term_meta($termId, '_glsr_reviews', true), 1);
     }
 
     public function test_assign_user()
     {
+        // automatically assign users
         $users = [
             self::factory()->user->create(),
             self::factory()->user->create(),
         ];
         $request = $this->request;
-        $request->set('assigned_users', implode(',', $users));
-        $review = $this->createReview($request);
+        $review = $this->createReview($request->merge([
+            'assigned_users' => implode(',', $users),
+        ]));
         $this->assertTrue($review->isValid());
         $this->assertEquals($review->assigned_users, $users);
+        foreach ($users as $userId) {
+            $this->assertEquals(get_user_meta($userId, '_glsr_average', true), 5);
+            $this->assertTrue(get_user_meta($userId, '_glsr_ranking', true) > 0);
+            $this->assertEquals(get_user_meta($userId, '_glsr_reviews', true), 1);
+        }
+        // manually assign user
         $userId = self::factory()->user->create();
         $users[] = $userId;
         glsr(ReviewManager::class)->assignUser($review, $userId);
         $review = glsr(ReviewManager::class)->get($review->ID);
         $this->assertEquals($review->assigned_users, $users);
+        $this->assertEquals(get_user_meta($userId, '_glsr_average', true), 5);
+        $this->assertTrue(get_user_meta($userId, '_glsr_ranking', true) > 0);
+        $this->assertEquals(get_user_meta($userId, '_glsr_reviews', true), 1);
     }
 
     public function test_create()
@@ -113,38 +146,53 @@ class ReviewManagerTest extends WP_UnitTestCase
     {
         $postId = self::factory()->post->create();
         $request = $this->request;
-        $request->set('assigned_posts', $postId);
-        $review = $this->createReview($request);
+        $review = $this->createReview($request->merge([
+            'assigned_posts' => $postId,
+        ]));
         $this->assertTrue($review->isValid());
         $this->assertEquals($review->assigned_posts, [$postId]);
+        // unassign post
         glsr(ReviewManager::class)->unassignPost($review, $postId);
         $review = glsr(ReviewManager::class)->get($review->ID);
         $this->assertEquals($review->assigned_posts, []);
+        $this->assertEquals(get_post_meta($postId, '_glsr_average', true), 0);
+        $this->assertEquals(get_post_meta($postId, '_glsr_ranking', true), 0);
+        $this->assertEquals(get_post_meta($postId, '_glsr_reviews', true), 0);
     }
 
     public function test_unassign_term()
     {
         $termId = self::factory()->term->create($this->termArgs);
         $request = $this->request;
-        $request->set('assigned_terms', $termId);
-        $review = $this->createReview($request);
+        $review = $this->createReview($request->merge([
+            'assigned_terms' => $termId,
+        ]));
         $this->assertTrue($review->isValid());
         $this->assertEquals($review->assigned_terms, [$termId]);
+        // unassign term
         glsr(ReviewManager::class)->unassignTerm($review, $termId);
         $review = glsr(ReviewManager::class)->get($review->ID);
         $this->assertEquals($review->assigned_terms, []);
+        $this->assertEquals(get_term_meta($termId, '_glsr_average', true), 0);
+        $this->assertEquals(get_term_meta($termId, '_glsr_ranking', true), 0);
+        $this->assertEquals(get_term_meta($termId, '_glsr_reviews', true), 0);
     }
 
     public function test_unassign_user()
     {
         $userId = self::factory()->user->create();
         $request = $this->request;
-        $request->set('assigned_users', $userId);
-        $review = $this->createReview($request);
+        $review = $this->createReview($request->merge([
+            'assigned_users' => $userId,
+        ]));
         $this->assertTrue($review->isValid());
         $this->assertEquals($review->assigned_users, [$userId]);
+        // unassign user
         glsr(ReviewManager::class)->unassignUser($review, $userId);
         $review = glsr(ReviewManager::class)->get($review->ID);
         $this->assertEquals($review->assigned_users, []);
+        $this->assertEquals(get_user_meta($userId, '_glsr_average', true), 0);
+        $this->assertEquals(get_user_meta($userId, '_glsr_ranking', true), 0);
+        $this->assertEquals(get_user_meta($userId, '_glsr_reviews', true), 0);
     }
 }
