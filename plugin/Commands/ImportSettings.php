41017579b56f0d776e18da3c7a6766f032bda3e6
Paul Ryley <paul@geminilabs.io>
2020-09-08 20:14:41 -0500

    import extends the Upload class

--- plugin/Commands/ImportSettings.php
+++ plugin/Commands/ImportSettings.php
@@ -4,46 +4,17 @@ namespace GeminiLabs\SiteReviews\Commands;
 
 use GeminiLabs\SiteReviews\Contracts\CommandContract as Contract;
 use GeminiLabs\SiteReviews\Database\OptionManager;
-use GeminiLabs\SiteReviews\Helpers\Arr;
-use GeminiLabs\SiteReviews\Helpers\Str;
 use GeminiLabs\SiteReviews\Modules\Notice;
+use GeminiLabs\SiteReviews\Upload;
 
-class ImportSettings implements Contract
+class ImportSettings extends Upload implements Contract
 {
-    private $file;
-
-    public function __construct()
-    {
-        $this->file = glsr()->args(Arr::get($_FILES, 'import-file', []));
-    }
-
-    /**
-     * @param int $errorCode
-     * @return string
-     */
-    public function getUploadError($errorCode)
-    {
-        $errors = [
-            UPLOAD_ERR_INI_SIZE => _x('The uploaded file exceeds the upload_max_filesize directive in php.ini.', 'admin-text', 'site-reviews'),
-            UPLOAD_ERR_FORM_SIZE => _x('The uploaded file is too big.', 'admin-text', 'site-reviews'),
-            UPLOAD_ERR_PARTIAL => _x('The uploaded file was only partially uploaded.', 'admin-text', 'site-reviews'),
-            UPLOAD_ERR_NO_FILE => _x('No file was uploaded.', 'admin-text', 'site-reviews'),
-            UPLOAD_ERR_NO_TMP_DIR => _x('Missing a temporary folder.', 'admin-text', 'site-reviews'),
-            UPLOAD_ERR_CANT_WRITE => _x('Failed to write file to disk.', 'admin-text', 'site-reviews'),
-            UPLOAD_ERR_EXTENSION => _x('A PHP extension stopped the file upload.', 'admin-text', 'site-reviews'),
-        ];
-        return Arr::get($errors, $errorCode, _x('Unknown upload error.', 'admin-text', 'site-reviews'));
-    }
-
     /**
      * @return void
      */
     public function handle()
     {
-        if (!$this->validateUpload()) {
-            return;
-        }
-        if (!$this->validateFileType()) {
+        if (!$this->validateUpload() || !$this->validateExtension('.json')) {
             return;
         }
         if ($this->import()) {
@@ -58,7 +29,7 @@ class ImportSettings implements Contract
      */
     protected function import()
     {
-        if ($settings = json_decode(file_get_contents($this->file->tmp_name), true)) {
+        if ($settings = json_decode(file_get_contents($this->file()->tmp_name), true)) { // @todo catch Exception if an invalid json file
             glsr(OptionManager::class)->set(
                 glsr(OptionManager::class)->normalize($settings)
             );
@@ -67,32 +38,6 @@ class ImportSettings implements Contract
         glsr(Notice::class)->addWarning(
             _x('There were no settings found to import.', 'admin-text', 'site-reviews')
         );
-        return false;
-    }
-
-    /**
-     * @return bool
-     */
-    protected function validateFileType()
-    {
-        if ('application/json' === $this->file->type && Str::endsWith('.json', $this->file->name)) {
-            return true;
-        }
-        glsr(Notice::class)->addError(
-            _x('Please use a valid Site Reviews settings file.', 'admin-text', 'site-reviews')
-        );
-        return false;
-    }
-
-    /**
-     * @return bool
-     */
-    protected function validateUpload()
-    {
-        if (UPLOAD_ERR_OK === $this->file->error) {
-            return true;
-        }
-        glsr(Notice::class)->addError($this->getUploadError($this->file->error));
         return false;
     }
 }
