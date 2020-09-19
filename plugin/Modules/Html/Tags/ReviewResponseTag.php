e60472adaf379b1a032950b8b79464faf9a2024e
Paul Ryley <paul@geminilabs.io>
2020-09-19 15:13:21 -0500

    fix review response tag

--- plugin/Modules/Html/Tags/ReviewResponseTag.php
+++ plugin/Modules/Html/Tags/ReviewResponseTag.php
@@ -14,14 +14,11 @@ class ReviewResponseTag extends ReviewContentTag
         if (!$this->isHidden() && !empty(trim($value))) {
             $title = sprintf(__('Response from %s', 'site-reviews'), get_bloginfo('name'));
             $text = $this->normalizeText($value);
-            $response = glsr(Builder::class)->div($text, [
+            $response = glsr(Builder::class)->div([
                 'class' => 'glsr-review-response-inner',
                 'text' => sprintf('<p><strong>%s</strong></p><p>%s</p>', $title, $text),
             ]);
-            $background = glsr(Builder::class)->div([
-                'class' => 'glsr-review-response-background',
-            ]);
-            return $this->wrap($response.$background);
+            return $this->wrap($response);
         }
     }
 }
