45a88893b106505e1064eb8a295c4d64c7dcbccc
Paul Ryley <paul@geminilabs.io>
2020-09-21 22:09:00 -0500

    add Divi style

---
+++ views/partials/styles/divi/type-toggle.php
@@ -0,0 +1,9 @@
+<?php defined('WPINC') || die; ?>
+
+<span class="{{ class }}">
+    <label for="{{ id }}">{{ text }}</label>
+    <span class="glsr-toggle-switch">
+        {{ input }} &#8203; <!-- zero-space character used for alignment -->
+        <span class="glsr-toggle-track"></span>
+    </span>
+</span>
