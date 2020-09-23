49dfe286aef0df116ab8abcc9cd3805bb618bd8b
Paul Ryley <paul@geminilabs.io>
2020-09-22 22:34:30 -0500

    add twentynineteen style

---
+++ views/partials/styles/twentynineteen/reviews-form.php
@@ -0,0 +1,9 @@
+<?php defined('WPINC') || die; ?>
+
+<div class="glsr-form-wrap">
+    <form class="comment-form {{ class }}" method="post" enctype="multipart/form-data">
+        {{ fields }}
+        {{ response }}
+        {{ submit_button }}
+    </form>
+</div>
