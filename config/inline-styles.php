e500f60f7711a0b58ae412b2535a251c7d2346ab
Paul Ryley <paul@geminilabs.io>
2020-10-04 18:13:44 -0500

    move the stars back into the images dir
    don't break things for people!

--- config/inline-styles.php
+++ config/inline-styles.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    ':star-empty' => glsr()->url('assets/images/stars/star-empty.svg'),
-    ':star-error' => glsr()->url('assets/images/stars/star-error.svg'),
-    ':star-full' => glsr()->url('assets/images/stars/star-full.svg'),
-    ':star-half' => glsr()->url('assets/images/stars/star-half.svg'),
+    ':star-empty' => glsr()->url('assets/images/star-empty.svg'),
+    ':star-error' => glsr()->url('assets/images/star-error.svg'),
+    ':star-full' => glsr()->url('assets/images/star-full.svg'),
+    ':star-half' => glsr()->url('assets/images/star-half.svg'),
 ];
