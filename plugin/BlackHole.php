b14079fe700adb025bb098ed658d1e68b8bff7e2
Paul Ryley <paul@geminilabs.io>
2020-10-10 19:40:05 -0500

    improve the Blackhole

--- plugin/BlackHole.php
+++ plugin/BlackHole.php
@@ -2,8 +2,18 @@
 
 namespace GeminiLabs\SiteReviews;
 
-class BlackHole
+class BlackHole extends \ArrayObject
 {
+    public function __construct()
+    {
+        parent::__construct([]);
+    }
+
+    public function __get($property)
+    {
+        return;
+    }
+
     public function __call($method, $args)
     {
         glsr_log()->error("Call to a member function $method() on an unknown class");
