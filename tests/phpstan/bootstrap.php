1501bc813728a84b107392216b9165a2da558cf5
Paul Ryley <paul@geminilabs.io>
2020-10-23 04:24:30 -0500

    improve phpstan config

---
+++ tests/phpstan/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('ICL_SITEPRESS_VERSION', '0.0.0');
+define('WP_CONTENT_DIR', '.');
+define('WP_MEMORY_LIMIT', '1G');
+define('WP_PLUGIN_DIR', '.');
+define('WPINC', 'wp-includes');
