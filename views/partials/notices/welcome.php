bb40077c27e2da90aefd94183bf57f9da3c32b9e
Paul Ryley <paul@geminilabs.io>
2020-10-24 15:55:46 -0500

    tweak the migrate and welcome notice text

--- views/partials/notices/welcome.php
+++ views/partials/notices/welcome.php
@@ -1,8 +1,8 @@
 <div class="notice notice-info is-dismissible glsr-notice" data-dismiss="welcome">
     <p><?= $text; ?></p>
     <p>
-        <a class="components-button is-secondary" href="<?= admin_url('index.php?page='.glsr()->id.'-welcome#tab-whatsnew'); ?>">✨&nbsp;<?= _x('See what\'s new', 'admin-text', 'site-reviews'); ?></a>
+        <a class="components-button is-secondary is-small" href="<?= admin_url('index.php?page='.glsr()->id.'-welcome#tab-whatsnew'); ?>">✨&nbsp;<?= _x('See What\'s New', 'admin-text', 'site-reviews'); ?></a>
         &nbsp;
-        <a class="components-button is-secondary" href="<?= admin_url('index.php?page='.glsr()->id.'-welcome#tab-upgrade-guide'); ?>">📄&nbsp;<?= _x('Read the Upgrade Guide', 'admin-text', 'site-reviews'); ?></a>
+        <a href="<?= admin_url('index.php?page='.glsr()->id.'-welcome#tab-upgrade-guide'); ?>"><?= _x('Read the upgrade guide', 'admin-text', 'site-reviews'); ?> →</a>
     </p>
 </div>
