ab5577f8a67b663adc431c55f64c2ea340c6e6f6
Paul Ryley <paul@geminilabs.io>
2020-10-23 20:18:31 -0500

    re-word the migration notice

--- views/partials/notices/migrate.php
+++ views/partials/notices/migrate.php
@@ -1,3 +1,4 @@
 <div class="notice notice-warning is-dismissible glsr-notice" data-notice="migrate">
-    <p><?= sprintf(_x('A recent update may have broken your reviews. Please run the %s tool to fix it.', 'admin-text', 'site-reviews'), $action); ?></p>
+    <p><strong><?= _x('Database Update Required.', 'admin-text', 'site-reviews'); ?></strong></p>
+    <p><?= sprintf(_x('Please run the %s tool to update your database to the newest version, this should also fix any broken reviews.', 'admin-text', 'site-reviews'), $action); ?></p>
 </div>
