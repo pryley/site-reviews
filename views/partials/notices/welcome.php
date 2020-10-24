c0708e7e92f57268346bac1da494babb1e40317c
Paul Ryley <paul@geminilabs.io>
2020-10-23 20:28:31 -0500

    add a link to the upgrade guide on the welcome notice

--- views/partials/notices/welcome.php
+++ views/partials/notices/welcome.php
@@ -1,4 +1,8 @@
 <div class="notice notice-info is-dismissible glsr-notice" data-dismiss="welcome">
     <p><?= $text; ?></p>
-    <p><a class="components-button is-secondary" href="<?= admin_url('index.php?page='.glsr()->id.'-welcome#tab-whatsnew'); ?>">✨&nbsp;<?= _x('See what\'s new', 'admin-text', 'site-reviews'); ?></a></p>
+    <p>
+        <a class="components-button is-secondary" href="<?= admin_url('index.php?page='.glsr()->id.'-welcome#tab-whatsnew'); ?>">✨&nbsp;<?= _x('See what\'s new', 'admin-text', 'site-reviews'); ?></a>
+        &nbsp;
+        <a class="components-button is-secondary" href="<?= admin_url('index.php?page='.glsr()->id.'-welcome#tab-upgrade-guide'); ?>">📄&nbsp;<?= _x('Read the Upgrade Guide', 'admin-text', 'site-reviews'); ?></a>
+    </p>
 </div>
