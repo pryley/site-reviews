059e8106cc6789e4637bb6a4c09b6da3ba24581c
Paul Ryley <paul@geminilabs.io>
2020-11-01 17:53:47 -0600

    ensure uninstall.php runs independent of the plugin
    This still needs to be modified for Multisite

--- uninstall.php
+++ uninstall.php
@@ -2,34 +2,43 @@
 
 defined('WP_UNINSTALL_PLUGIN') || die;
 
-$file = __DIR__.'/site-reviews.php';
-require_once $file;
-
-if (!(new GL_Plugin_Check_v5($file))->isValid()) {
-    return;
-}
-
 global $wpdb;
 
-$uninstallOption = glsr_get_option('general.delete_data_on_uninstall');
+$app = array(
+    'id' => 'site-reviews',
+    'prefix' => 'glsr_',
+    'post_type' => 'site-review',
+    'taxonomy' => 'site-review-category',
+);
+$settings = get_option($app['id'].'-v5');
+$uninstall = isset($settings['general']['delete_data_on_uninstall'])
+    ? $settings['general']['delete_data_on_uninstall']
+    : '';
+$versions = array(
+    1 => 'geminilabs_site_reviews_settings',
+    2 => 'geminilabs_site_reviews-v2',
+    3 => $app['id'].'-v3',
+    4 => $app['id'].'-v4',
+    5 => $app['id'].'-v5',
+);
 
-if (in_array($uninstallOption, ['all', 'minimal'])) {
-    foreach (range(1, glsr()->version('major')) as $version) {
-        delete_option(GeminiLabs\SiteReviews\Database\OptionManager::databaseKey($version));
+if (in_array($uninstall, ['all', 'minimal'])) {
+    foreach ($versions as $version) {
+        delete_option($version);
     }
-    delete_option('theme_mods_'.glsr()->id);
-    delete_option('widget_'.glsr()->prefix.'site-reviews');
-    delete_option('widget_'.glsr()->prefix.'site-reviews-form');
-    delete_option('widget_'.glsr()->prefix.'site-reviews-summary');
-    delete_option(glsr()->prefix.'activated');
-    delete_transient(glsr()->prefix.'cloudflare_ips');
-    delete_transient(glsr()->prefix.'migrations');
-    delete_transient(glsr()->prefix.'remote_post_test');
+    delete_option('theme_mods_'.$app['id']);
+    delete_option('widget_'.$app['prefix'].'site-reviews');
+    delete_option('widget_'.$app['prefix'].'site-reviews-form');
+    delete_option('widget_'.$app['prefix'].'site-reviews-summary');
+    delete_option($app['prefix'].'activated');
+    delete_transient($app['prefix'].'cloudflare_ips');
+    delete_transient($app['prefix'].'migrations');
+    delete_transient($app['prefix'].'remote_post_test');
     $wpdb->query("DELETE FROM {$wpdb->usermeta} WHERE meta_key = '_glsr_notices'");
 }
-if ('all' === $uninstallOption) {
-    $likePrefix = '%'.$wpdb->esc_like(glsr()->prefix).'%';
-    $likeTaxonomy = '%'.$wpdb->esc_like(glsr()->taxonomy).'%';
+if ('all' === $uninstall) {
+    $likePrefix = '%'.$wpdb->esc_like($app['prefix']).'%';
+    $likeTaxonomy = '%'.$wpdb->esc_like($app['taxonomy']).'%';
     // delete all reviews and revisions
     $wpdb->query($wpdb->prepare("
         DELETE p, pr, tr, pm
@@ -37,7 +46,7 @@ if ('all' === $uninstallOption) {
         LEFT JOIN {$wpdb->posts} pr ON (p.ID = pr.post_parent AND pr.post_type = 'revision')
         LEFT JOIN {$wpdb->term_relationships} tr ON (p.ID = tr.object_id)
         LEFT JOIN {$wpdb->postmeta} pm ON (p.ID = pm.post_id)
-        WHERE p.post_type = %s", glsr()->post_type
+        WHERE p.post_type = %s", $app['post_type']
     ));
     // delete all review categories
     $wpdb->query($wpdb->prepare("
@@ -45,7 +54,7 @@ if ('all' === $uninstallOption) {
         FROM {$wpdb->term_taxonomy} tt
         LEFT JOIN {$wpdb->terms} t ON (tt.term_id = t.term_id)
         LEFT JOIN {$wpdb->termmeta} tm ON (tt.term_id = tm.term_id)
-        WHERE tt.taxonomy = %s", glsr()->taxonomy
+        WHERE tt.taxonomy = %s", $app['taxonomy']
     ));
     // delete all assigned_posts meta
     $wpdb->query($wpdb->prepare("DELETE FROM {$wpdb->postmeta} WHERE meta_key LIKE %s", $likePrefix));
@@ -55,7 +64,7 @@ if ('all' === $uninstallOption) {
     $wpdb->query($wpdb->prepare("DELETE FROM {$wpdb->options} WHERE option_name LIKE %s", $likePrefix));
     $wpdb->query($wpdb->prepare("DELETE FROM {$wpdb->options} WHERE option_name LIKE %s", $likeTaxonomy));
     // drop all custom tables
-    $prefix = $wpdb->prefix.glsr()->prefix;
+    $prefix = $wpdb->prefix.$prefix;
     $wpdb->query("DROP TABLE {$prefix}assigned_posts");
     $wpdb->query("DROP TABLE {$prefix}assigned_terms");
     $wpdb->query("DROP TABLE {$prefix}assigned_users");
@@ -68,7 +77,7 @@ if ('all' === $uninstallOption) {
     $wpdb->query("OPTIMIZE TABLE {$wpdb->terms}");
     $wpdb->query("OPTIMIZE TABLE {$wpdb->usermeta}");
     // delete the saved database version
-    delete_option(glsr()->prefix.'db_version');
+    delete_option($app['prefix'].'db_version');
     // finally, flush the entire cache
     wp_cache_flush();
 }
