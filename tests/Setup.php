ed597d9f0b3fbdb248001b2cea9e02fc2a7acc81
Paul Ryley <paul@geminilabs.io>
2020-11-02 15:22:27 -0600

    fix unit tests

--- tests/Setup.php
+++ tests/Setup.php
@@ -15,7 +15,7 @@ trait Setup
         $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
         $_SERVER['SERVER_NAME'] = '';
         $this->referer = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
-        glsr()->activate();
+        glsr(Install::class)->run();
         glsr(OptionManager::class)->set(glsr(DefaultsManager::class)->get());
         // save initial plugin settings here if needed
     }
