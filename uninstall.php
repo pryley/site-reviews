855557fca11da17dbf7d8299cc45c5578c670d0a
Paul Ryley <paul@geminilabs.io>
2020-11-02 17:44:15 -0600

    add multisite support to uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -2,75 +2,32 @@
 
 defined('WP_UNINSTALL_PLUGIN') || die;
 
-global $wpdb;
-
-$app = array(
-    'id' => 'site-reviews',
-    'prefix' => 'glsr_',
-    'post_type' => 'site-review',
-    'taxonomy' => 'site-review-category',
-);
-$settings = get_option($app['id'].'-v5');
-$uninstall = isset($settings['general']['delete_data_on_uninstall'])
-    ? $settings['general']['delete_data_on_uninstall']
-    : '';
-$versions = array(
-    1 => 'geminilabs_site_reviews_settings',
-    2 => 'geminilabs_site_reviews-v2',
-    3 => $app['id'].'-v3',
-    4 => $app['id'].'-v4',
-    5 => $app['id'].'-v5',
-);
-
-wp_clear_scheduled_hook('site-reviews/schedule/session/purge'); // @removed in v5.2
-
-if (in_array($uninstall, ['all', 'minimal'])) {
-    foreach ($versions as $version) {
-        delete_option($version);
+function glsr_uninstall() {
+    $settings = get_option('site-reviews-v5');
+    $uninstall = isset($settings['general']['delete_data_on_uninstall'])
+        ? $settings['general']['delete_data_on_uninstall']
+        : '';
+    if ('all' === $uninstall) {
+        glsr_uninstall_all();
     }
-    delete_option('theme_mods_'.$app['id']);
-    delete_option('widget_'.$app['prefix'].'site-reviews');
-    delete_option('widget_'.$app['prefix'].'site-reviews-form');
-    delete_option('widget_'.$app['prefix'].'site-reviews-summary');
-    delete_option($app['prefix'].'activated');
-    delete_transient($app['prefix'].'cloudflare_ips');
-    delete_transient($app['prefix'].'migrations');
-    delete_transient($app['prefix'].'remote_post_test');
-    $wpdb->query("DELETE FROM {$wpdb->usermeta} WHERE meta_key = '_glsr_notices'");
+    if ('minimal' === $uninstall) {
+        glsr_uninstall_minimal();
+    }
 }
-if ('all' === $uninstall) {
-    $likePrefix = '%'.$wpdb->esc_like($app['prefix']).'%';
-    $likeTaxonomy = '%'.$wpdb->esc_like($app['taxonomy']).'%';
-    // delete all reviews and revisions
-    $wpdb->query($wpdb->prepare("
-        DELETE p, pr, tr, pm
-        FROM {$wpdb->posts} p
-        LEFT JOIN {$wpdb->posts} pr ON (p.ID = pr.post_parent AND pr.post_type = 'revision')
-        LEFT JOIN {$wpdb->term_relationships} tr ON (p.ID = tr.object_id)
-        LEFT JOIN {$wpdb->postmeta} pm ON (p.ID = pm.post_id)
-        WHERE p.post_type = %s", $app['post_type']
-    ));
-    // delete all review categories
-    $wpdb->query($wpdb->prepare("
-        DELETE tt, t, tm
-        FROM {$wpdb->term_taxonomy} tt
-        LEFT JOIN {$wpdb->terms} t ON (tt.term_id = t.term_id)
-        LEFT JOIN {$wpdb->termmeta} tm ON (tt.term_id = tm.term_id)
-        WHERE tt.taxonomy = %s", $app['taxonomy']
-    ));
-    // delete all assigned_posts meta
-    $wpdb->query($wpdb->prepare("DELETE FROM {$wpdb->postmeta} WHERE meta_key LIKE %s", $likePrefix));
-    // delete all assigned_users meta
-    $wpdb->query($wpdb->prepare("DELETE FROM {$wpdb->usermeta} WHERE meta_key LIKE %s", $likePrefix));
+
+function glsr_uninstall_all() {
+    glsr_uninstall_minimal();
+    glsr_uninstall_all_delete_reviews();
+    glsr_uninstall_all_delete_tables();
+    glsr_uninstall_all_delete_logs();
+    glsr_uninstall_all_cleanup();
+}
+
+function glsr_uninstall_all_cleanup() {
+    global $wpdb;
     // delete any remaining options
-    $wpdb->query($wpdb->prepare("DELETE FROM {$wpdb->options} WHERE option_name LIKE %s", $likePrefix));
-    $wpdb->query($wpdb->prepare("DELETE FROM {$wpdb->options} WHERE option_name LIKE %s", $likeTaxonomy));
-    // drop all custom tables
-    $prefix = $wpdb->prefix.$prefix;
-    $wpdb->query("DROP TABLE {$prefix}assigned_posts");
-    $wpdb->query("DROP TABLE {$prefix}assigned_terms");
-    $wpdb->query("DROP TABLE {$prefix}assigned_users");
-    $wpdb->query("DROP TABLE {$prefix}ratings");
+    $wpdb->query("DELETE FROM {$wpdb->options} WHERE option_name LIKE '%glsr_%'");
+    $wpdb->query("DELETE FROM {$wpdb->options} WHERE option_name LIKE 'site-review-category%'");
     // optimise affected database tables
     $wpdb->query("OPTIMIZE TABLE {$wpdb->options}");
     $wpdb->query("OPTIMIZE TABLE {$wpdb->postmeta}");
@@ -78,8 +35,91 @@ if ('all' === $uninstall) {
     $wpdb->query("OPTIMIZE TABLE {$wpdb->termmeta}");
     $wpdb->query("OPTIMIZE TABLE {$wpdb->terms}");
     $wpdb->query("OPTIMIZE TABLE {$wpdb->usermeta}");
-    // delete the saved database version
-    delete_option($app['prefix'].'db_version');
     // finally, flush the entire cache
     wp_cache_flush();
+}
+
+function glsr_uninstall_all_delete_logs() {
+    require_once ABSPATH.'/wp-admin/includes/file.php';
+    global $wp_filesystem;
+    // delete the Site Reviews logs directory
+    if (WP_Filesystem()) {
+        $uploads = wp_upload_dir();
+        $dirname = trailingslashit($uploads['basedir'].'/site-reviews/logs');
+        $wp_filesystem->rmdir(wp_normalize_path($dirname), true);
+    }
+}
+
+function glsr_uninstall_all_delete_reviews() {
+    global $wpdb;
+    // delete all reviews and revisions
+    $wpdb->query("
+        DELETE p, pr, tr, pm
+        FROM {$wpdb->posts} p
+        LEFT JOIN {$wpdb->posts} pr ON (p.ID = pr.post_parent AND pr.post_type = 'revision')
+        LEFT JOIN {$wpdb->term_relationships} tr ON (p.ID = tr.object_id)
+        LEFT JOIN {$wpdb->postmeta} pm ON (p.ID = pm.post_id)
+        WHERE p.post_type = 'site-review'
+    ");
+    // delete all review categories
+    $wpdb->query("
+        DELETE tt, t, tm
+        FROM {$wpdb->term_taxonomy} tt
+        LEFT JOIN {$wpdb->terms} t ON (tt.term_id = t.term_id)
+        LEFT JOIN {$wpdb->termmeta} tm ON (tt.term_id = tm.term_id)
+        WHERE tt.taxonomy = 'site-review-category'
+    ");
+    // delete all assigned_posts meta
+    $wpdb->query("DELETE FROM {$wpdb->postmeta} WHERE meta_key LIKE '%glsr_%'");
+    // delete all assigned_users meta
+    $wpdb->query("DELETE FROM {$wpdb->usermeta} WHERE meta_key LIKE '%glsr_%'");
+}
+
+function glsr_uninstall_all_delete_tables() {
+    global $wpdb;
+    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}glsr_assigned_posts");
+    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}glsr_assigned_terms");
+    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}glsr_assigned_users");
+    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}glsr_ratings");
+    // delete the saved database version
+    delete_option('glsr_db_version');
+}
+
+function glsr_uninstall_minimal() {
+    global $wpdb;
+    $options = array(
+        'geminilabs_site_reviews-v2', // v2 settings
+        'geminilabs_site_reviews_settings', // v1 settings
+        'glsr_activated',
+        'site-reviews-v3', // v3 settings
+        'site-reviews-v4', // v4 settings
+        'site-reviews-v5', // v5 settings
+        'theme_mods_site-reviews',
+        'widget_glsr_site-reviews',
+        'widget_glsr_site-reviews-form',
+        'widget_glsr_site-reviews-summary',
+    );
+    foreach ($options as $option) {
+        delete_option($option);
+    }
+    delete_transient('glsr_cloudflare_ips');
+    delete_transient('glsr_migrations');
+    delete_transient('glsr_remote_post_test');
+    $wpdb->query("DELETE FROM {$wpdb->usermeta} WHERE meta_key = '_glsr_notices'");
+}
+
+if (!is_multisite()) {
+    glsr_uninstall();
+    return;
+}
+if (!function_exists('get_sites')) {
+    global $wpdb;
+    $siteIds = $wpdb->get_col("SELECT blog_id FROM {$wpdb->blogs}");
+} else {
+    $siteIds = get_sites(array('fields' => 'ids'));
+}
+foreach ($siteIds as $siteId) {
+    switch_to_blog($siteId);
+    glsr_uninstall();
+    restore_current_blog();
 }
