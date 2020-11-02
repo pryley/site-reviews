d9b4e2d214f8e92ed87a852e02967339a86a51dc
Paul Ryley <paul@geminilabs.io>
2020-11-01 19:44:15 -0600

    add Install class

---
+++ plugin/Install.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace GeminiLabs\SiteReviews;
+
+use GeminiLabs\SiteReviews\Database\SqlSchema;
+
+class Install
+{
+    /**
+     * @return void
+     */
+    public function run()
+    {
+        if (is_plugin_active_for_network(glsr()->file)) {
+            $sites = get_sites([
+                'fields' => 'ids',
+                'network_id' => get_current_network_id(),
+            ]);
+            foreach ($sites as $siteId) {
+                $this->runOnSite($siteId);
+            }
+            return;
+        }
+        $this->install();
+    }
+
+    /**
+     * @param int $siteId
+     * @return void
+     */
+    public function runOnSite($siteId)
+    {
+        switch_to_blog($siteId);
+        $this->install();
+        restore_current_blog();
+    }
+
+    /**
+     * @return void
+     */
+    protected function install()
+    {
+        $this->createRoleCapabilities();
+        $this->createTables();
+        $this->scheduleCronJob();
+    }
+
+    /**
+     * @return void
+     */
+    protected function createRoleCapabilities()
+    {
+        glsr(Role::class)->resetAll();
+    }
+
+    /**
+     * @return void
+     */
+    protected function createTables()
+    {
+        glsr(SqlSchema::class)->createTables();
+        glsr(SqlSchema::class)->addTableConstraints();
+    }
+
+    /**
+     * @return void
+     */
+    protected function scheduleCronJob()
+    {
+        wp_clear_scheduled_hook(glsr()->cron_event);
+        wp_schedule_event(time() + MINUTE_IN_SECONDS, 'daily', glsr()->cron_event);
+    }
+}
