c7c20144b3be8db40a6f67460c9deb6473d7347d
Paul Ryley <paul@geminilabs.io>
2020-11-03 19:25:15 -0600

    add dropTables method to Install class

--- plugin/Install.php
+++ plugin/Install.php
@@ -2,10 +2,39 @@
 
 namespace GeminiLabs\SiteReviews;
 
+use GeminiLabs\SiteReviews\Database;
+use GeminiLabs\SiteReviews\Database\Query;
 use GeminiLabs\SiteReviews\Database\SqlSchema;
 
 class Install
 {
+    /**
+     * @param bool $dropAll
+     * @return void
+     */
+    public function dropTables($dropAll = true)
+    {
+        $tables = $this->tables();
+        if (is_multisite() && $dropAll) {
+            $sites = get_sites([
+                'fields' => 'ids',
+                'network_id' => get_current_network_id(),
+            ]);
+            foreach ($sites as $siteId) {
+                switch_to_blog($siteId);
+                $tables = array_unique(array_merge($tables, $this->tables()));
+                delete_option('glsr_db_version');
+                restore_current_blog();
+            }
+        }
+        foreach ($tables as $table) {
+            glsr(Database::class)->dbQuery(
+                glsr(Query::class)->sql("DROP TABLE IF EXISTS {$table}")
+            );
+        }
+        delete_option('glsr_db_version');
+    }
+
     /**
      * @return void
      */
@@ -60,5 +89,18 @@ class Install
     {
         $this->createRoleCapabilities();
         $this->createTables();
+    }
+
+    /**
+     * @return array
+     */
+    protected function tables()
+    {
+        return [
+            glsr(SqlSchema::class)->table('assigned_posts'),
+            glsr(SqlSchema::class)->table('assigned_terms'),
+            glsr(SqlSchema::class)->table('assigned_users'),
+            glsr(SqlSchema::class)->table('ratings'),
+        ];
     }
 }
