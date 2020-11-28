29ef8301cd3678eadb56fe19eece8c7e7311d68a
Paul Ryley <paul@geminilabs.io>
2020-11-27 22:27:00 -0600

    fix toggle styles

--- views/partials/styles/divi/type-toggle.php
+++
@@ -1,9 +0,0 @@
-<?php defined('ABSPATH') || die; ?>
-
-<span class="{{ class }}">
-    <label for="{{ id }}">{{ text }}</label>
-    <span class="glsr-toggle-switch">
-        {{ input }} &#8203; <!-- zero-space character used for alignment -->
-        <span class="glsr-toggle-track"></span>
-    </span>
-</span>
