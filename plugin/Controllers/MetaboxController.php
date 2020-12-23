d2f2707f3fc0a28520e57d724323db682ba0cf10
Paul Ryley <paul@geminilabs.io>
2020-12-22 19:14:57 -0600

    allow the target attribute in response links

--- plugin/Controllers/MetaboxController.php
+++ plugin/Controllers/MetaboxController.php
@@ -205,7 +205,7 @@ class MetaboxController
         }
         $response = strval(Helper::filterInput('response'));
         $response = trim(wp_kses($response, [
-            'a' => ['href' => [], 'title' => []],
+            'a' => ['href' => [], 'target' => [], 'title' => []],
             'em' => [],
             'strong' => [],
         ]));
