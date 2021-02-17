0fb1daba894695b40956aabc462fe36f2144ef56
Paul Ryley <paul@geminilabs.io>
2021-02-17 01:14:20 -0600

    refactor StarRating partial

--- plugin/Modules/Html/Partials/StarRating.php
+++ plugin/Modules/Html/Partials/StarRating.php
@@ -3,6 +3,7 @@
 namespace GeminiLabs\SiteReviews\Modules\Html\Partials;
 
 use GeminiLabs\SiteReviews\Contracts\PartialContract;
+use GeminiLabs\SiteReviews\Defaults\StarRatingDefaults;
 use GeminiLabs\SiteReviews\Modules\Html\Template;
 use GeminiLabs\SiteReviews\Modules\Rating;
 
@@ -11,45 +12,31 @@ class StarRating implements PartialContract
     /**
      * @var \GeminiLabs\SiteReviews\Arguments
      */
-    protected $args;
-
-    /**
-     * @var int
-     */
-    protected $count;
-
-    /**
-     * @var string
-     */
-    protected $prefix;
-
-    /**
-     * @var float
-     */
-    protected $rating;
+    public $data;
 
     /**
      * {@inheritdoc}
      */
     public function build(array $data = [])
     {
-        $this->setProperties($data);
+        $this->data = glsr()->args(glsr(StarRatingDefaults::class)->merge($data));
         $maxRating = glsr()->constant('MAX_RATING', Rating::class);
-        $fullStars = intval(floor($this->rating));
-        $halfStars = intval(ceil($this->rating - $fullStars));
+        $fullStars = intval(floor($this->data->rating));
+        $halfStars = intval(ceil($this->data->rating - $fullStars));
         $emptyStars = max(0, $maxRating - $fullStars - $halfStars);
-        $title = $this->count > 0
+        $title = $this->data->count > 0
             ? __('Rated <strong>%s</strong> out of %s based on %s ratings', 'site-reviews')
             : __('Rated <strong>%s</strong> out of %s', 'site-reviews');
         return glsr(Template::class)->build('templates/rating/stars', [
-            'args' => $this->args,
+            'args' => glsr()->args($this->data->args),
             'context' => [
                 'empty_stars' => $this->getTemplate('empty-star', $emptyStars),
                 'full_stars' => $this->getTemplate('full-star', $fullStars),
                 'half_stars' => $this->getTemplate('half-star', $halfStars),
-                'prefix' => $this->prefix,
-                'title' => sprintf($title, $this->rating, $maxRating, $this->count),
+                'prefix' => $this->data->prefix,
+                'title' => sprintf($title, $this->data->rating, $maxRating, $this->data->count),
             ],
+            'partial' => $this,
         ]);
     }
 
@@ -61,28 +48,12 @@ class StarRating implements PartialContract
     protected function getTemplate($templateName, $timesRepeated)
     {
         $template = glsr(Template::class)->build('templates/rating/'.$templateName, [
-            'args' => $this->args,
+            'args' => $this->data->args,
             'context' => [
-                'prefix' => $this->prefix,
+                'prefix' => $this->data->prefix,
             ],
+            'partial' => $this,
         ]);
         return str_repeat($template, $timesRepeated);
-    }
-
-    /**
-     * @return void
-     */
-    protected function setProperties(array $data)
-    {
-        $data = wp_parse_args($data, [
-            'args' => [],
-            'count' => 0,
-            'prefix' => glsr()->isAdmin() ? '' : 'glsr-',
-            'rating' => 0,
-        ]);
-        $this->args = glsr()->args($data['args']);
-        $this->count = (int) $data['count'];
-        $this->prefix = $data['prefix'];
-        $this->rating = (float) sprintf('%g', $data['rating']); // remove unnecessary trailing zeros
     }
 }
