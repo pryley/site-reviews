1237749de013fad82d1a2ade82e72127b9dd2003
Paul Ryley <paul@geminilabs.io>
2021-06-09 14:34:35 -0400

    update changelog

---
+++ views/pages/welcome/whatsnew/v512.php
@@ -0,0 +1,20 @@
+<div class="glsr-card postbox is-fullwidth open">
+    <h3 class="glsr-card-heading">
+        <button type="button" class="glsr-accordion-trigger" aria-expanded="true" aria-controls="welcome-v5_12_0">
+            <span class="title">Version 5.12</span>
+            <span class="icon"></span>
+        </button>
+    </h3>
+    <div id="welcome-v5_12_0" class="inside">
+        <p><em>Initial Release Date &mdash; June 7th, 2021</em></p>
+        <h4>✨ New Features</h4>
+        <ul>
+            <li>Added the "rating_field" shortcode option which allows you to use a custom rating field with the summary (the <a href="<?= admin_url('edit.php?post_type='.glsr()->post_type.'&page=addons'); ?>">Review Forms</a> add-on is required to add custom fields)</li>
+            <li>Added the "Repair Review Relations" tool</li>
+        </ul>
+        <h4>🐞 Bugs Fixed</h4>
+        <ul>
+            <li>Fixed "loose assignment" database queries</li>
+        </ul>
+    </div>
+</div>
