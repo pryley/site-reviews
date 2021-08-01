e1843f02a757b7245f58a7965a7fbeab91ea97f8
Paul Ryley <paul@geminilabs.io>
2021-08-01 19:26:08 -0400

    Use singleton controllers to allow people to remove hooks

--- plugin/Provider.php
+++ plugin/Provider.php
@@ -3,8 +3,25 @@
 namespace GeminiLabs\SiteReviews;
 
 use GeminiLabs\SiteReviews\Contracts\ProviderContract;
+use GeminiLabs\SiteReviews\Controllers\AdminController;
+use GeminiLabs\SiteReviews\Controllers\Api\Version1\RestController;
+use GeminiLabs\SiteReviews\Controllers\BlocksController;
+use GeminiLabs\SiteReviews\Controllers\EditorController;
+use GeminiLabs\SiteReviews\Controllers\IntegrationController;
+use GeminiLabs\SiteReviews\Controllers\ListTableController;
 use GeminiLabs\SiteReviews\Controllers\MainController;
-use GeminiLabs\SiteReviews\Database\OptionManager;
+use GeminiLabs\SiteReviews\Controllers\MenuController;
+use GeminiLabs\SiteReviews\Controllers\MetaboxController;
+use GeminiLabs\SiteReviews\Controllers\NoticeController;
+use GeminiLabs\SiteReviews\Controllers\PrivacyController;
+use GeminiLabs\SiteReviews\Controllers\PublicController;
+use GeminiLabs\SiteReviews\Controllers\ReviewController;
+use GeminiLabs\SiteReviews\Controllers\RevisionController;
+use GeminiLabs\SiteReviews\Controllers\SettingsController;
+use GeminiLabs\SiteReviews\Controllers\TaxonomyController;
+use GeminiLabs\SiteReviews\Controllers\ToolsController;
+use GeminiLabs\SiteReviews\Controllers\TranslationController;
+use GeminiLabs\SiteReviews\Controllers\WelcomeController;
 use GeminiLabs\SiteReviews\Modules\Style;
 use GeminiLabs\SiteReviews\Modules\Translation;
 use GeminiLabs\SiteReviews\Modules\Translator;
@@ -23,6 +40,26 @@ class Provider implements ProviderContract
         $app->singleton(Style::class);
         $app->singleton(Translator::class);
         $app->singleton(Translation::class);
-        $app->singleton(MainController::class); // this goes last
+        // Using controllers as singletons allow people to remove hooks!
+        $app->singleton(AdminController::class);
+        $app->singleton(BlocksController::class);
+        $app->singleton(EditorController::class);
+        $app->singleton(IntegrationController::class);
+        $app->singleton(ListTableController::class);
+        $app->singleton(MainController::class);
+        $app->singleton(MenuController::class);
+        $app->singleton(MetaboxController::class);
+        $app->singleton(NoticeController::class);
+        $app->singleton(PrivacyController::class);
+        $app->singleton(PublicController::class);
+        $app->singleton(RestController::class);
+        $app->singleton(ReviewController::class);
+        $app->singleton(RevisionController::class);
+        $app->singleton(Router::class);
+        $app->singleton(SettingsController::class);
+        $app->singleton(TaxonomyController::class);
+        $app->singleton(ToolsController::class);
+        $app->singleton(TranslationController::class);
+        $app->singleton(WelcomeController::class);
     }
 }
