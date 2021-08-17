5ff0968790eafc429af73a0b955af782f5934d17
Paul Ryley <paul@geminilabs.io>
2021-08-17 10:11:03 +0200

    allow using custom templates with Email class

--- plugin/Modules/Email.php
+++ plugin/Modules/Email.php
@@ -167,9 +167,9 @@ class Email
         }
         $context = ['context' => $this->email['template-tags']];
         $template = trim(glsr(OptionManager::class)->get('settings.general.notification_message'));
-        if (!empty($template)) {
+        if ('email-notification' === $this->email['template'] && !empty($template)) {
             return glsr(Template::class)->interpolate($template, $this->email['template'], $context);
-        } elseif ($this->email['template']) {
+        } elseif (!empty($this->email['template'])) {
             return glsr(Template::class)->build('templates/'.$this->email['template'], $context);
         }
         return '';
