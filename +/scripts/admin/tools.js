82bc141438a49cdb5598f484310adf07f6ba1a05
Paul Ryley <paul@geminilabs.io>
2021-11-15 16:04:51 -0400

    fix compatibility with gamipress

--- +/scripts/admin/tools.js
+++ +/scripts/admin/tools.js
@@ -3,8 +3,8 @@
 import Ajax from './ajax.js';
 
 const Tools = function () {
-    jQuery('form').on('click', '#clear-console', this.loadConsole_, this.onClick_.bind(this));
-    jQuery('form').on('click', '#fetch-console', this.loadConsole_, this.onClick_.bind(this));
+    jQuery('form').on('click', '#glsr-clear-console', this.loadConsole_, this.onClick_.bind(this));
+    jQuery('form').on('click', '#glsr-fetch-console', this.loadConsole_, this.onClick_.bind(this));
     jQuery('form').on('click', '[data-ajax-click]', this.onClick_.bind(this));
     jQuery('.glsr-button').on('click', this.setExpand_);
     var alt = jQuery('input[data-alt]');
@@ -17,7 +17,7 @@ const Tools = function () {
 Tools.prototype = {
     loadConsole_: function (response, success) {
         if (success) {
-            jQuery('#log-file').val(response.console);
+            jQuery('#glsr-log-file').val(response.console);
         }
     },
     onClick_: function (ev) {
