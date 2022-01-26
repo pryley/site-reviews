b4672deaa57af62fdd3b9c15173cbb50a60c5571
Paul Ryley <paul@geminilabs.io>
2022-01-26 18:02:05 -0500

    fix the category filter visibility in the All Reviews table

--- plugin/Defaults/ColumnFilterbyDefaults.php
+++ plugin/Defaults/ColumnFilterbyDefaults.php
@@ -14,6 +14,7 @@ class ColumnFilterbyDefaults extends Defaults
         return [
             'assigned_post' => FILTER_SANITIZE_NUMBER_INT,
             'assigned_user' => FILTER_SANITIZE_NUMBER_INT,
+            'category' => FILTER_SANITIZE_NUMBER_INT,
             'rating' => FILTER_SANITIZE_NUMBER_INT,
             'type' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
         ];
