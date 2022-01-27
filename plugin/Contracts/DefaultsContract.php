7900bed21dcbce3770d0fc2a40e3c45537ee8e54
Paul Ryley <paul@geminilabs.io>
2022-01-27 00:50:00 -0500

    v5.20.0 release

--- plugin/Contracts/DefaultsContract.php
+++ plugin/Contracts/DefaultsContract.php
@@ -2,6 +2,14 @@
 
 namespace GeminiLabs\SiteReviews\Contracts;
 
+/**
+ * @property array $casts
+ * @property array $concatenated
+ * @property array $enum
+ * @property array $guarded
+ * @property array $mapped
+ * @property array $sanitize
+ */
 interface DefaultsContract
 {
 }
