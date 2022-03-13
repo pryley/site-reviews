31946f7d0f58079b786c0c85f4681b34b0736c9e
Paul Ryley <paul@geminilabs.io>
2022-03-13 17:49:03 -0400

    Request::inputGet()

--- plugin/Request.php
+++ plugin/Request.php
@@ -2,6 +2,23 @@
 
 namespace GeminiLabs\SiteReviews;
 
+use GeminiLabs\SiteReviews\Helpers\Arr;
+use GeminiLabs\SiteReviews\Helpers\Cast;
+
 class Request extends Arguments
 {
+    /**
+     * @return static
+     * @todo support array values
+     */
+    public static function inputGet()
+    {
+        $values = Arr::consolidate(filter_input_array(INPUT_GET));
+        foreach ($values as &$value) {
+            if (!is_numeric($value)) {
+                $value = sanitize_text_field(trim(Cast::toString($value)));
+            }
+        }
+        return new static($values);
+    }
 }
