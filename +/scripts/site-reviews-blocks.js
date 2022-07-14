05ad36704feb7b300f42e009133170360c597ac9
Paul Ryley <paul@geminilabs.io>
2022-07-14 20:53:09 +0100

    fix block errors in customizer widgets

--- +/scripts/site-reviews-blocks.js
+++ +/scripts/site-reviews-blocks.js
@@ -1,3 +1,8 @@
 import "./blocks/block-form";
 import "./blocks/block-reviews";
 import "./blocks/block-summary";
+import Event from './public/event.js';
+
+if (!window.hasOwnProperty('GLSR')) {
+    window.GLSR = { Event };
+}
