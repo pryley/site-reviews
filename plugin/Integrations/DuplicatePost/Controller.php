8a4359e92c14f43e6b0680060176ddab817dc2d3
Paul Ryley <paul@geminilabs.io>
2022-07-22 12:00:20 +0100

    fix compatibility with Yoast Duplicate Post

---
+++ plugin/Integrations/DuplicatePost/Controller.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace GeminiLabs\SiteReviews\Integrations\DuplicatePost;
+
+use GeminiLabs\SiteReviews\Controllers\Controller as BaseController;
+use GeminiLabs\SiteReviews\Database\ReviewManager;
+use GeminiLabs\SiteReviews\Helpers\Arr;
+use GeminiLabs\SiteReviews\Helpers\Str;
+use GeminiLabs\SiteReviews\Review;
+
+class Controller extends BaseController
+{
+    /**
+     * @param int|\WP_Error $newPostId
+     * @param \WP_Post $post
+     * @param string $status
+     * @action duplicate_post_post_copy
+     */
+    public function duplicateReview($newPostId, $post, $status)
+    {
+        if (!Review::isReview($post)) {
+            return;
+        }
+        $review = glsr_get_review($post->ID);
+        if ($review->isValid()) {
+            glsr(ReviewManager::class)->createFromPost($newPostId, $review->toArray());
+        }
+    }
+
+    /**
+     * @param string[] $actions
+     * @return array
+     * @filter bulk_actions-edit-{Application::POST_TYPE}
+     */
+    public function removeRewriteBulkAction($actions)
+    {
+        unset($actions['duplicate_post_bulk_rewrite_republish']);
+        return $actions;
+    }
+
+    /**
+     * @param \WP_Post|null $post
+     * @action post_submitbox_start
+     */
+    public function removeRewriteEditorLink($post)
+    {
+        if (!Review::isReview($post)) {
+            return;
+        }
+        global $wp_filter;
+        $callbacks = Arr::get($wp_filter, 'post_submitbox_start.callbacks.10', []);
+        foreach ($callbacks as $key => $value) {
+            if (Str::endsWith('add_rewrite_and_republish_post_button', $key)) {
+                remove_action('post_submitbox_start', Arr::get($value, 'function'), 10);
+            }
+        }
+    }
+
+    /**
+     * @param string[] $actions
+     * @param \WP_Post $post
+     * @return array
+     * @filter post_row_actions
+     */
+    public function removeRewriteRowAction($actions, $post)
+    {
+        if (Review::isReview($post)) {
+            unset($actions['rewrite']);
+        }
+        return $actions;
+    }
+}
---
+++ plugin/Integrations/DuplicatePost/Hooks.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace GeminiLabs\SiteReviews\Integrations\DuplicatePost;
+
+use GeminiLabs\SiteReviews\Contracts\HooksContract;
+
+class Hooks implements HooksContract
+{
+    /**
+     * @var Controller
+     */
+    public $controller;
+
+    public function __construct()
+    {
+        $this->controller = glsr(Controller::class);
+    }
+
+    /**
+     * @return void
+     */
+    public function run()
+    {
+        add_action('duplicate_post_post_copy', [$this->controller, 'duplicateReview'], 10, 3);
+        add_filter('bulk_actions-edit-'.glsr()->post_type, [$this->controller, 'removeRewriteBulkAction'], 100);
+        add_action('post_submitbox_start', [$this->controller, 'removeRewriteEditorLink'], 1);
+        add_filter('post_row_actions', [$this->controller, 'removeRewriteRowAction'], 100, 2);
+    }
+}
