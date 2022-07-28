d0ae06ad6afb90066fe9a6a1743d4b76f07f5b4e
Paul Ryley <paul@geminilabs.io>
2022-07-28 18:48:06 +0100

    rewrite debounce function

--- +/scripts/public/helpers.js
+++ +/scripts/public/helpers.js
@@ -12,6 +12,14 @@ export const classListSelector = (classValue) => { // string
     return '.' + classValue.trim().split(' ').join('.');
 };
 
+export const debounce = (fn, wait = 200) => {
+    let timeoutId = null
+    return (...args) => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(fn, wait, ...args);
+    }
+}
+
 /** @return object */
 export const extend = () => { // ...object
     var args = [].slice.call(arguments);
@@ -25,33 +33,3 @@ export const extend = () => { // ...object
     });
     return result;
 };
-
-export function debounce (fn, wait = 200) {
-    let timerId;
-    let lastArgs;
-    let lastCallTime;
-    let lastThis;
-    let result;
-    const startTimer = (pendingFunc) => {
-        cancelAnimationFrame(timerId)
-        return requestAnimationFrame(pendingFunc)
-    }
-    const timerExpired = () => {
-        const timeSinceLastCall = performance.now() - lastCallTime;
-        if (timeSinceLastCall >= wait || timeSinceLastCall < 0) {
-            result = fn.apply(lastThis, lastArgs);
-            lastArgs = lastThis = void 0;
-            return result
-        }
-        timerId = startTimer(timerExpired);
-    }
-    return (...args) => {
-        lastArgs = args;
-        lastCallTime = performance.now();
-        lastThis = this;
-        if (timerId === void 0) {
-            timerId = startTimer(timerExpired);
-        }
-        return result
-    }
-}
