98cb065f2aab4eb82f0da8834a04dd88b7272caa
Paul Ryley <paul@geminilabs.io>
2022-08-01 21:37:38 +0100

    wrap buttons in wp-block-button classes

--- templates/form/submit-button.php
+++ templates/form/submit-button.php
@@ -1,8 +1,11 @@
 <?php defined('ABSPATH') || die; ?>
 
+
 <div data-field="submit-button">
-    <button type="submit" class="glsr-button button btn btn-primary" data-loading="{{ text }}" aria-busy="false">
-        <span class="glsr-button-loading"></span>
-        <span class="glsr-button-text">{{ text }}</span>
-    </button>
+    <div class="wp-block-button">
+        <button type="submit" class="wp-block-button__link glsr-button button btn btn-primary" data-loading="{{ text }}" aria-busy="false">
+            <span class="glsr-button-loading"></span>
+            <span class="glsr-button-text">{{ text }}</span>
+        </button>
+    </div>
 </div>
--- templates/load-more-button.php
+++ templates/load-more-button.php
@@ -1,6 +1,8 @@
 <?php defined('ABSPATH') || die; ?>
 
-<button type="button" class="glsr-button-loadmore glsr-button button btn btn-primary" data-loading="{{ loading_text }}" data-page="{{ page }}" aria-busy="false" aria-label="{{ screen_reader_text }}">
-    <span class="glsr-button-loading"></span>
-    <span class="glsr-button-text">{{ text }}</span>
-</button>
+<div class="wp-block-button">
+    <button type="button" class="wp-block-button__link glsr-button-loadmore glsr-button button btn btn-primary" data-loading="{{ loading_text }}" data-page="{{ page }}" aria-busy="false" aria-label="{{ screen_reader_text }}">
+        <span class="glsr-button-loading"></span>
+        <span class="glsr-button-text">{{ text }}</span>
+    </button>
+</div>
