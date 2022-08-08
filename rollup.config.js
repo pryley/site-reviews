d72a57f615714b2368be419649f30ab728940976
Paul Ryley <paul@geminilabs.io>
2022-08-08 14:56:19 +0100

    update star-rating.js and rewrite styles using CSS variables (unfinished)

--- rollup.config.js
+++ rollup.config.js
@@ -1,77 +1,135 @@
-
+import alias from '@rollup/plugin-alias';
 import babel from '@rollup/plugin-babel';
 import filesize from 'rollup-plugin-filesize';
+import path from 'path';
+import postcss from 'rollup-plugin-postcss'
 import resolve from '@rollup/plugin-node-resolve';
-import { terser } from "rollup-plugin-terser";
+import { terser } from 'rollup-plugin-terser';
 
-const production = process.env.NODE_ENV === 'production';
+const pluginsJavascript = () => [
+  alias({ entries: {'@': path.resolve(__dirname, '+/scripts/')} }),
+  resolve(),
+  filesize(),
+  babel({
+    babelHelpers: 'runtime',
+    plugins: [
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-transform-runtime',
+    ],
+    presets: [
+      '@babel/preset-env',
+    ],
+  }),
+  terser({
+    compress: {
+      pure_funcs: Object.keys(console)
+        .filter(key => !~['info', 'warn', 'error'].indexOf(key))
+        .map(key => `console.${key}`),
+    },
+    format: { comments: false },
+    // mangle: {
+    //   reserved: [
+    //     'close',
+    //     'destroy',
+    //     'disableButton',
+    //     'enableButton',
+    //     'events',
+    //     'execute',
+    //     'get',
+    //     'init',
+    //     'off',
+    //     'on',
+    //     'once',
+    //     'open',
+    //     'post',
+    //     'render',
+    //     'reset',
+    //     'setErrors',
+    //     'submitForm',
+    //     'toggleError',
+    //     'trigger',
+    //     'validate',
+    //   ],
+    //   // properties: true,
+    //   properties: {
+    //     regex: /^_/,
+    //   },
+    // },
+  }),
+  // require('rollup-plugin-sizes')({ details: true }),
+  // require('rollup-plugin-visualizer').visualizer({ template: 'sunburst' }),
+];
 
-const babelConfig = {
-  babelHelpers: 'runtime',
-  plugins: [
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-transform-runtime",
-  ],
-  presets: [
-    "@babel/preset-env"
-  ],
+const pluginsStylesheet = (namespace) => {
+  return [
+    postcss({
+      extract: true,
+      minimize: false,
+      plugins: [
+        require('postcss-import'),
+        require('postcss-preset-env')({
+          features: { 'custom-properties': false },
+          stage: 1,
+        }),
+        require('postcss-calc'),
+        require('postcss-selector-namespace')({ namespace }),
+      ],
+    }),
+  ];
 }
 
-const pureFuncs = Object
-  .keys(console)
-  .filter(key => !~['info', 'warn', 'error'].indexOf(key))
-  .map(key => `console.${key}`);
-
-export default [
-  {
-    input: {
-      'site-reviews': '+/scripts/site-reviews.js',
-    },
-    output: {
-      dir: 'assets/scripts',
-      entryFileNames: '[name].js',
-      format: 'iife',
-    },
-    plugins: [
-      resolve(),
-      filesize(),
-      babel(babelConfig),
-      terser({
-        compress: {
-          pure_funcs: pureFuncs,
-        },
-        format: {
-          comments: false,
-        },
-        // mangle: {
-        //   reserved: [
-        //     'close',
-        //     'destroy',
-        //     'disableButton',
-        //     'enableButton',
-        //     'events',
-        //     'execute',
-        //     'get',
-        //     'init',
-        //     'off',
-        //     'on',
-        //     'once',
-        //     'open',
-        //     'post',
-        //     'render',
-        //     'reset',
-        //     'setErrors',
-        //     'submitForm',
-        //     'toggleError',
-        //     'trigger',
-        //     'validate',
-        //   ],
-        //   // properties: true,
-        //   properties: {
-        //     regex: /^_/,
-        //   },
-        // },
-      }),
-    ]
+const css = (id, namespace = '', dir = 'assets/styles') => ({
+  input: `+/styles/${id}.css`,
+  plugins: pluginsStylesheet(namespace),
+  output: {
+    file: path.resolve(__dirname, `${dir}/${id}.css`),
   },
+  onwarn (warning, warn) {
+    if (warning.code === 'FILE_NAME_CONFLICT') return
+    warn(warning)
+  }
+})
+
+const js = (id) => ({
+  input: `+/scripts/${id}.js`,
+  plugins: pluginsJavascript(),
+  output: {
+    dir: 'assets/scripts',
+    format: 'iife',
+  },
+})
+
+module.exports = [
+  js('site-reviews', ),
+  // css('inline-styles'),
+  // css('bootstrap_4', '.glsr-bootstrap_4'),
+  // css('bootstrap_4_custom', '.glsr-bootstrap_4_custom'),
+  // css('contact_form_7', '.glsr-contact_form_7'),
+  // css('default', '.glsr-default'),
+  // css('divi', '.et-db #et-main-area .glsr-divi, .et-db #et-boc .glsr-divi, .glsr-divi'),
+  // css('minimal', '.glsr-minimal'),
+  // css('ninja_forms', '.glsr-ninja_forms'),
+  // css('twentyfifteen', '.glsr-twentyfifteen'),
+  // css('twentysixteen', '.glsr-twentysixteen'),
+  // css('twentyseventeen', '.glsr-twentyseventeen'),
+  // css('twentynineteen', '.glsr-twentynineteen'),
+  // css('twentytwenty', '.glsr-twentytwenty'),
+  // css('twentytwentyone', '.glsr-twentytwentyone'),
+  // css('twentytwentytwo', '.glsr-twentytwentytwo'),
+  // css('wpforms', '.glsr-wpforms'),
+  // css('bootstrap_4-blocks', '[data-block]', 'assets/styles/blocks'),
+  // css('bootstrap_4_custom-blocks', '[data-block]', 'assets/styles/blocks'),
+  // css('contact_form_7-blocks', '[data-block]', 'assets/styles/blocks'),
+  // css('default-blocks', '[data-block]', 'assets/styles/blocks'),
+  // css('divi-blocks', '[data-block]', 'assets/styles/blocks'),
+  // css('minimal-blocks', '[data-block]', 'assets/styles/blocks'),
+  // css('ninja_forms-blocks', '[data-block]', 'assets/styles/blocks'),
+  // css('twentyfifteen-blocks', '[data-block]', 'assets/styles/blocks'),
+  // css('twentysixteen-blocks', '[data-block]', 'assets/styles/blocks'),
+  // css('twentyseventeen-blocks', '[data-block]', 'assets/styles/blocks'),
+  // css('twentynineteen-blocks', '[data-block]', 'assets/styles/blocks'),
+  // css('twentytwenty-blocks', '[data-block]', 'assets/styles/blocks'),
+  // css('twentytwentyone-blocks', '[data-block]', 'assets/styles/blocks'),
+  // css('twentytwentytwo-blocks', '[data-block]', 'assets/styles/blocks'),
+  // css('wpforms-blocks', '[data-block]', 'assets/styles/blocks'),
 ];
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,11 +3,13 @@ const path = require('path')
 const postCss = namespace => {
   return [
     require('postcss-import'),
-    require('postcss-preset-env')({stage: 0}),
-    require('postcss-calc')({preserve: false}),
+    require('postcss-preset-env')({
+      features: { 'custom-properties': false },
+      stage: 1,
+    }),
+    require('postcss-calc'),
     require('postcss-hexrgba'),
-    require('postcss-custom-properties')({preserve: false}),
-    require('postcss-selector-namespace')({namespace: namespace}),
+    require('postcss-selector-namespace')({ namespace }),
     require('autoprefixer'),
   ]
 }
@@ -61,41 +63,41 @@ mix.webpackConfig({
 mix
   .babel('+/scripts/mce-plugin.js', 'assets/scripts/mce-plugin.js')
   .babel('+/scripts/rollback.js', 'assets/scripts/rollback.js')
-  // .js('+/scripts/site-reviews.js', 'assets/scripts')
+  .js('+/scripts/site-reviews.js', 'assets/scripts')
   .js('+/scripts/site-reviews-admin.js', 'assets/scripts')
   .js('+/scripts/site-reviews-blocks.js', 'assets/scripts')
   .sass('+/styles/admin.scss', 'assets/styles/admin')
   .postCss('+/styles/inline-styles.css', 'assets/styles', postCss())
-  .postCss('+/styles/bootstrap_4.css', 'assets/styles', postCss('.glsr-bootstrap_4'))
-  .postCss('+/styles/bootstrap_4_custom.css', 'assets/styles', postCss('.glsr-bootstrap_4_custom'))
-  .postCss('+/styles/contact_form_7.css', 'assets/styles', postCss('.glsr-contact_form_7'))
-  .postCss('+/styles/default.css', 'assets/styles', postCss('.glsr-default'))
-  .postCss('+/styles/divi.css', 'assets/styles', postCss('.et-db #et-main-area .glsr-divi, .et-db #et-boc .glsr-divi, .glsr-divi'))
+  // .postCss('+/styles/bootstrap_4.css', 'assets/styles', postCss('.glsr-bootstrap_4'))
+  // .postCss('+/styles/bootstrap_4_custom.css', 'assets/styles', postCss('.glsr-bootstrap_4_custom'))
+  // .postCss('+/styles/contact_form_7.css', 'assets/styles', postCss('.glsr-contact_form_7'))
+  // .postCss('+/styles/default.css', 'assets/styles', postCss('.glsr-default'))
+  // .postCss('+/styles/divi.css', 'assets/styles', postCss('.et-db #et-main-area .glsr-divi, .et-db #et-boc .glsr-divi, .glsr-divi'))
   .postCss('+/styles/minimal.css', 'assets/styles', postCss('.glsr-minimal'))
-  .postCss('+/styles/ninja_forms.css', 'assets/styles', postCss('.glsr-ninja_forms'))
+  // .postCss('+/styles/ninja_forms.css', 'assets/styles', postCss('.glsr-ninja_forms'))
   .postCss('+/styles/twentyfifteen.css', 'assets/styles', postCss('.glsr-twentyfifteen'))
-  .postCss('+/styles/twentynineteen.css', 'assets/styles', postCss('.glsr-twentynineteen'))
-  .postCss('+/styles/twentyseventeen.css', 'assets/styles', postCss('.glsr-twentyseventeen'))
   .postCss('+/styles/twentysixteen.css', 'assets/styles', postCss('.glsr-twentysixteen'))
+  .postCss('+/styles/twentyseventeen.css', 'assets/styles', postCss('.glsr-twentyseventeen'))
+  .postCss('+/styles/twentynineteen.css', 'assets/styles', postCss('.glsr-twentynineteen'))
   .postCss('+/styles/twentytwenty.css', 'assets/styles', postCss('.glsr-twentytwenty'))
   .postCss('+/styles/twentytwentyone.css', 'assets/styles', postCss('.glsr-twentytwentyone'))
   .postCss('+/styles/twentytwentytwo.css', 'assets/styles', postCss('.glsr-twentytwentytwo'))
-  .postCss('+/styles/wpforms.css', 'assets/styles', postCss('.glsr-wpforms'))
-  .postCss('+/styles/bootstrap_4-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
-  .postCss('+/styles/bootstrap_4_custom-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
-  .postCss('+/styles/contact_form_7-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
-  .postCss('+/styles/default-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
-  .postCss('+/styles/divi-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
-  .postCss('+/styles/minimal-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
-  .postCss('+/styles/ninja_forms-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
-  .postCss('+/styles/twentyfifteen-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
-  .postCss('+/styles/twentynineteen-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
-  .postCss('+/styles/twentyseventeen-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
-  .postCss('+/styles/twentysixteen-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
-  .postCss('+/styles/twentytwenty-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
-  .postCss('+/styles/twentytwentyone-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
-  .postCss('+/styles/twentytwentytwo-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
-  .postCss('+/styles/wpforms-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
+  // .postCss('+/styles/wpforms.css', 'assets/styles', postCss('.glsr-wpforms'))
+  // .postCss('+/styles/bootstrap_4-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
+  // .postCss('+/styles/bootstrap_4_custom-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
+  // .postCss('+/styles/contact_form_7-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
+  // .postCss('+/styles/default-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
+  // .postCss('+/styles/divi-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
+  // .postCss('+/styles/minimal-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
+  // .postCss('+/styles/ninja_forms-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
+  // .postCss('+/styles/twentyfifteen-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
+  // .postCss('+/styles/twentysixteen-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
+  // .postCss('+/styles/twentyseventeen-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
+  // .postCss('+/styles/twentynineteen-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
+  // .postCss('+/styles/twentytwenty-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
+  // .postCss('+/styles/twentytwentyone-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
+  // .postCss('+/styles/twentytwentytwo-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
+  // .postCss('+/styles/wpforms-blocks.css', 'assets/styles/blocks', postCss('[data-block]'))
   .browserSync({
     files: [
         '+/**/*.(css|js)', 
