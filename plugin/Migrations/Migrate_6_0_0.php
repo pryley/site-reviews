7e8ff08096d251b71a091f1feeaa925709d6d763
Paul Ryley <paul@geminilabs.io>
2022-08-14 16:19:37 +0100

    add Review Filters migration to v6.0
    since the add-ons do not provide migrations

--- plugin/Migrations/Migrate_6_0_0.php
+++ plugin/Migrations/Migrate_6_0_0.php
@@ -16,8 +16,19 @@ class Migrate_6_0_0 implements MigrateContract
         while ($version) {
             delete_option(OptionManager::databaseKey($version--));
         }
+        $this->migrateAddonBlocks();
         return true;
     }
 
-
+    protected function migrateAddonBlocks(): void
+    {
+        if (glsr()->addon('site-reviews-filters')) {
+            global $wpdb;
+            $wpdb->query("
+                UPDATE {$wpdb->posts} p
+                SET p.post_content = REPLACE(p.post_content, '<!-- wp:site-reviews/filter ', '<!-- wp:site-reviews/filters ')
+                WHERE p.post_status = 'publish'
+            ");
+        }
+    }
 }
