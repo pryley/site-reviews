4e7527595890172cdc6e8f143411429b3c0bb857
Paul Ryley <paul@geminilabs.io>
2022-08-30 21:10:51 +0100

    allow hiding the verified tag

--- plugin/Shortcodes/SiteReviewsShortcode.php
+++ plugin/Shortcodes/SiteReviewsShortcode.php
@@ -71,7 +71,7 @@ class SiteReviewsShortcode extends Shortcode
      */
     protected function hideOptions()
     {
-        return [
+        return [ // order is intentional
             'title' => _x('Hide the title', 'admin-text', 'site-reviews'),
             'rating' => _x('Hide the rating', 'admin-text', 'site-reviews'),
             'date' => _x('Hide the date', 'admin-text', 'site-reviews'),
@@ -79,6 +79,7 @@ class SiteReviewsShortcode extends Shortcode
             'content' => _x('Hide the content', 'admin-text', 'site-reviews'),
             'avatar' => _x('Hide the avatar (if shown)', 'admin-text', 'site-reviews'),
             'author' => _x('Hide the author', 'admin-text', 'site-reviews'),
+            'verified' => _x('Hide the verified badge', 'admin-text', 'site-reviews'),
             'response' => _x('Hide the response', 'admin-text', 'site-reviews'),
         ];
     }
