9bdb9ccff27f0a374179485a1d856ad974088482
Paul Ryley <paul@geminilabs.io>
2022-10-10 23:53:50 +0200

    add migration

---
+++ plugin/Migrations/Migrate_6_1_0.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace GeminiLabs\SiteReviews\Migrations;
+
+use GeminiLabs\SiteReviews\Contracts\MigrateContract;
+use GeminiLabs\SiteReviews\Database\DefaultsManager;
+use GeminiLabs\SiteReviews\Database\OptionManager;
+use GeminiLabs\SiteReviews\Helpers\Arr;
+use GeminiLabs\SiteReviews\Helpers\Str;
+
+class Migrate_6_1_0 implements MigrateContract
+{
+    /**
+     * Run migration.
+     */
+    public function run(): bool
+    {
+        $this->migrateSettings();
+        return true;
+    }
+
+    /**
+     * Remove invalid settings
+     */
+    public function migrateSettings(): void
+    {
+        if (6 !== (int) glsr()->version('major')) {
+            return;
+        }
+        $defaults = glsr()->filterArray('get/defaults', glsr(DefaultsManager::class)->get());
+        $settings = get_option(OptionManager::databaseKey());
+        if (empty($defaults['settings']) || empty($settings['settings'])) {
+            return;
+        }
+        $defaultSettings = Arr::flatten($defaults['settings']);
+        $dirtySettings = Arr::flatten($settings['settings']);
+        $cleanSettings = [];
+        foreach ($dirtySettings as $key => $values) {
+            if (Str::startsWith($key, ['addons', 'licenses', 'strings'])) {
+                $cleanSettings[$key] = $values;
+                continue;
+            }
+            if (array_key_exists($key, $defaultSettings)) {
+                $cleanSettings[$key] = $values;
+            }
+        }
+        if (!empty($cleanSettings)) {
+            $settings['settings'] = Arr::convertFromDotNotation($cleanSettings);
+            update_option(OptionManager::databaseKey(), $settings);
+        }
+    }
+}
