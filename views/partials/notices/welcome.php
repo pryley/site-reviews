8414a64831efdac73e5cbd8916644b64d7a47f77
Paul Ryley <paul@geminilabs.io>
2022-10-12 00:19:52 +0200

    fix welcome notice links for new users

--- views/partials/notices/welcome.php
+++ views/partials/notices/welcome.php
@@ -4,7 +4,7 @@
     <p><?= $text; ?></p>
     <p class="glsr-notice-buttons">
         <?php if ($fresh) { ?>
-            <a class="button" href="<?= glsr_admin_url('documentation', 'support'); ?>" data-expand="#support-get-started"><?= _x("Start Here", 'admin-text', 'site-reviews'); ?></a>
+            <a class="button button-primary" href="<?= glsr_admin_url('welcome'); ?>"><?= _x("Start Here", 'admin-text', 'site-reviews'); ?></a>
             <a class="button button-link" href="<?= glsr_admin_url('documentation', 'shortcodes'); ?>"><?= _x('Read the Shortcode Documentation', 'admin-text', 'site-reviews'); ?> →</a>
         <?php } else { ?>
             <a class="button" href="<?= glsr_admin_url('welcome', 'whatsnew'); ?>"><?= _x("See What's New", 'admin-text', 'site-reviews'); ?></a>
