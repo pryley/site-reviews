a9e20a035a06e70cd4197a34f4f054fffda1d6a1
Paul Ryley <paul@geminilabs.io>
2022-11-20 16:22:14 +0100

    add notice to Scheduled Actions tab

--- views/pages/tools/scheduled.php
+++ views/pages/tools/scheduled.php
@@ -1,4 +1,12 @@
-<?php defined('ABSPATH') || exit;
+<?php defined('ABSPATH') || exit; ?>
+
+<div class="components-notice is-info" style="margin:20px 0 2px;">
+    <p class="components-notice__content">
+        <?= _x('Site Reviews uses a versatile library called <a href="https://actionscheduler.org" target="_blank">Action Scheduler</a> to manage and schedule cron jobs in WordPress. Action Scheduler improves your site’s overall ability to process large tasks. Many other popular WordPress plugins like WooCommerce, RankMath, WPForms, and All in One SEO also rely on Action Scheduler to help execute actions more efficiently.', 'admin-text', 'site-reviews'); ?>
+    </p>
+</div>
+
+<?php
 
 /**
  * This hack allow the list table to set the correct URLs
