6cc26bd3f5b57c040d31443e2851af0e50473216
Paul Ryley <paul@geminilabs.io>
2023-01-20 15:34:51 -0500

    fix wp_star_rating usage when max_rating > 5

--- plugin/Controllers/ListTableColumns/ColumnValueRating.php
+++ plugin/Controllers/ListTableColumns/ColumnValueRating.php
@@ -3,6 +3,7 @@
 namespace GeminiLabs\SiteReviews\Controllers\ListTableColumns;
 
 use GeminiLabs\SiteReviews\Contracts\ColumnValueContract;
+use GeminiLabs\SiteReviews\Modules\Rating;
 use GeminiLabs\SiteReviews\Review;
 
 class ColumnValueRating implements ColumnValueContract
@@ -12,9 +13,16 @@ class ColumnValueRating implements ColumnValueContract
      */
     public function handle(Review $review)
     {
-        return wp_star_rating([
-            'echo' => false,
-            'rating' => $review->rating,
-        ]);
+        $maxRating = glsr()->constant('MAX_RATING', Rating::class);
+        if (5 === (int) $maxRating) {
+            return wp_star_rating([
+                'echo' => false,
+                'rating' => $review->rating,
+            ]);
+        }
+        return sprintf('<span style="background:#787c82;border-radius:4px;color:#fff;padding:4px 8px;">%s / %s',
+            $review->rating,
+            $maxRating
+        );
     }
 }
