969f54aebb2b355dce749d8a9eed3f4ad29b58ee
Paul Ryley <paul@geminilabs.io>
2023-02-21 23:11:19 -0400

    update action scheduler to v3.5.4

--- update-action-scheduler.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ROOT_PATH=$(pwd)"/"
-
-cd $ROOT_PATH
-git fetch subtree-action-scheduler master
-git subtree pull --prefix vendors/woocommerce/action-scheduler subtree-action-scheduler master --squash
