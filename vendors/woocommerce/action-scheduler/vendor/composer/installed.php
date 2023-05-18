3e3a361cd79bced66109d8b4801081e8a380172e
Paul Ryley <paul@geminilabs.io>
2023-05-18 18:36:18 +0200

    update action scheduler to v3.6.0

---
+++ vendors/woocommerce/action-scheduler/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'woocommerce/action-scheduler',
+        'pretty_version' => 'dev-trunk',
+        'version' => 'dev-trunk',
+        'reference' => '1373570a9ee03a0be31c73df086d1e4343a27347',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'woocommerce/action-scheduler' => array(
+            'pretty_version' => 'dev-trunk',
+            'version' => 'dev-trunk',
+            'reference' => '1373570a9ee03a0be31c73df086d1e4343a27347',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
