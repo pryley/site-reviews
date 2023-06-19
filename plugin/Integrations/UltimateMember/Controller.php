e7102636e40e17b0038a0997751fb8ce0c923e08
Paul Ryley <paul@geminilabs.io>
2023-06-19 15:52:59 +0200

    add Ultimate Member compatibility

---
+++ plugin/Integrations/UltimateMember/Controller.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace GeminiLabs\SiteReviews\Integrations\UltimateMember;
+
+use GeminiLabs\SiteReviews\Controllers\Controller as BaseController;
+use GeminiLabs\SiteReviews\Review;
+
+class Controller extends BaseController
+{
+    /**
+     * @action site-reviews/avatar/generate
+     */
+    public function filterAvatarUrl(string $url, int $size, Review $review): string
+    {
+        $type = glsr_get_option('reviews.avatars_fallback');
+        $defaultUrl = um_get_default_avatar_uri(); // @phpstan-ignore-line
+        if ('none' === $type && !$review->author_id) {
+            return $defaultUrl;
+        }
+        if ('none' !== $type && $url === $defaultUrl) {
+            return '';
+        }
+        return $url;
+    }
+}
---
+++ plugin/Integrations/UltimateMember/Hooks.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace GeminiLabs\SiteReviews\Integrations\UltimateMember;
+
+use GeminiLabs\SiteReviews\Hooks\AbstractHooks;
+
+class Hooks extends AbstractHooks
+{
+    public function run(): void
+    {
+        if (!function_exists('um_get_default_avatar_uri')) {
+            return;
+        }
+        $this->hook(Controller::class, [
+            ['filterAvatarUrl', 'site-reviews/avatar/generate', 10, 3],
+        ]);
+    }
+}
