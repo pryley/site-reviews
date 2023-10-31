4990dedd44b285f3831ad5a5fb9ab86960fd9f8f
Paul Ryley <paul@geminilabs.io>
2023-10-31 16:06:22 +0100

    fix WooRewards integration again

--- plugin/Integrations/WooRewards/Hooks.php
+++ plugin/Integrations/WooRewards/Hooks.php
@@ -14,14 +14,17 @@ class Hooks extends AbstractHooks
         }
         add_action('lws_woorewards_abstracts_event_installed', function () {
             $callback = glsr(Compatibility::class)->findCallback('comment_post', 'trigger', '\LWS\WOOREWARDS\Events\ProductReview');
-            if ($callback) {
-                glsr()->store('\LWS\WOOREWARDS\Events\ProductReview', $callback);
-                glsr(Compatibility::class)->removeHook('comment_post', 'trigger', '\LWS\WOOREWARDS\Events\ProductReview');
-                glsr(Compatibility::class)->removeHook('comment_unapproved_to_approved', 'delayedApproval', '\LWS\WOOREWARDS\Events\ProductReview');
-                glsr(Compatibility::class)->removeHook('wp_insert_comment', 'review', '\LWS\WOOREWARDS\Events\ProductReview');
-                add_action('site-reviews/review/approved', [$this->controller, 'onApprovedReview'], 20);
-                add_action('site-reviews/review/created', [$this->controller, 'onCreatedReview'], 20);
+            if (empty($callback)) {
+                return;
             }
+            glsr()->store('\LWS\WOOREWARDS\Events\ProductReview', $callback);
+            glsr(Compatibility::class)->removeHook('comment_post', 'trigger', '\LWS\WOOREWARDS\Events\ProductReview');
+            glsr(Compatibility::class)->removeHook('comment_unapproved_to_approved', 'delayedApproval', '\LWS\WOOREWARDS\Events\ProductReview');
+            glsr(Compatibility::class)->removeHook('wp_insert_comment', 'review', '\LWS\WOOREWARDS\Events\ProductReview');
+            $this->hook(Controller::class, [
+                ['onApprovedReview', 'site-reviews/review/approved', 20],
+                ['onCreatedReview', 'site-reviews/review/created', 20],
+            ]);
         });
     }
 }
