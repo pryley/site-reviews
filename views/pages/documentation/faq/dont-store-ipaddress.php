74498253a671bd00211910d5d3d2adc4113ac06d
Paul Ryley <paul@geminilabs.io>
2023-12-08 15:00:11 +0100

    add GDPR context to IP Address FAQ

--- views/pages/documentation/faq/dont-store-ipaddress.php
+++ views/pages/documentation/faq/dont-store-ipaddress.php
@@ -8,6 +8,12 @@
         </button>
     </h3>
     <div id="faq-dont-store-ipaddress" class="inside">
+        <div class="glsr-notice-inline components-notice is-info">
+            <p class="components-notice__content">
+                <a href="https://en.wikipedia.org/wiki/General_Data_Protection_Regulation">GDPR</a> EU law does not require you to obtain consent to log an IP address if the purpose is of legitimate interest. Logging IP addresses for the purpose of security is an extremely widespread practice and is a legitimate interest to comply with standard security practices. See also: <a href="https://law.stackexchange.com/a/28609">https://law.stackexchange.com/a/28609</a>.
+            </p>
+        </div>
+        <p>Site Reviews saves the IP address to the review when it is submitted for security purposes only (i.e. to prevent spam and automated reviews). If you do not want the IP address logged then you can use the following code snippet:</p>
         <pre><code class="language-php">/**
  * Prevents the IP address from being saved with the review
  * Paste this code in your theme's functions.php file.
