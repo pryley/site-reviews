20a6d198048c4cfea520cb8d2fc71a1db395583b
Paul Ryley <paul@geminilabs.io>
2024-01-22 23:35:29 -0500

    fix dashboard widget style

--- views/partials/dashboard-widget.php
+++ views/partials/dashboard-widget.php
@@ -18,15 +18,23 @@
 </ul>
 
 <p class="glsr-dashboard__footer">
-    <span><?= _x('Version', 'admin-text', 'site-reviews'); ?> <?= glsr()->version; ?></span>
-    <a href="<?= glsr_admin_url('documentation'); ?>">
-        <?= _x('Help', 'admin-text', 'site-reviews'); ?>
-    </a>
-    <a href="https://wordpress.org/support/plugin/site-reviews" target="_blank">
-        <?= _x('Support Forum', 'admin-text', 'site-reviews'); ?>
-        <span class="screen-reader-text">
-            <?= _x('(opens in a new tab)', 'admin-text', 'site-reviews'); ?>
-        </span>
-        <span aria-hidden="true" class="dashicons dashicons-external" style="font-size:18px;vertical-align:-3px;"></span>
-    </a>
+    <span>
+        <a href="<?= glsr_admin_url('tools', 'system-info'); ?>">
+            <?= _x('Version', 'admin-text', 'site-reviews'); ?> <?= glsr()->version; ?>
+        </a>
+    </span>
+    <span>
+        <a href="<?= glsr_admin_url('documentation'); ?>">
+            <?= _x('Help', 'admin-text', 'site-reviews'); ?>
+        </a>
+    </span>
+    <span>
+        <a href="https://wordpress.org/support/plugin/site-reviews" target="_blank">
+            <?= _x('Support Forum', 'admin-text', 'site-reviews'); ?>
+            <span class="screen-reader-text">
+                <?= _x('(opens in a new tab)', 'admin-text', 'site-reviews'); ?>
+            </span>
+            <span aria-hidden="true" class="dashicons dashicons-external"></span>
+        </a>
+    </span>
 </p>
