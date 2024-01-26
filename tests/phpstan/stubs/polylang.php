178d3e5c348d24f2d9fbb7d6f2edf63225865fba
Paul Ryley <paul@geminilabs.io>
2024-01-25 20:56:45 -0500

    add missing polylang stub methods

--- tests/phpstan/stubs/polylang.php
+++ tests/phpstan/stubs/polylang.php
@@ -14,6 +14,20 @@ namespace {
     function pll_get_post($post_id, $lang = '')
     {}
     /**
+     * Among the term and its translations, returns the ID of the term which is in the language represented by $lang.
+     *
+     * @api
+     * @since 0.5
+     * @since 3.4 Returns 0 instead of false.
+     * @since 3.4 $lang accepts PLL_Language or string.
+     *
+     * @param int                 $term_id Term ID.
+     * @param PLL_Language|string $lang    Optional language (object or slug), defaults to the current language.
+     * @return int|false The translation term ID if exists, otherwise the passed ID. False if the passed object has no language or if the language doesn't exist.
+     */
+    function pll_get_term($term_id, $lang = null)
+    {}
+    /**
      * Returns the post language.
      *
      * @api
@@ -24,6 +38,22 @@ namespace {
      * @return string|false The requested field for the post language, false if no language is associated to that post.
      */
     function pll_get_post_language($post_id, $field = 'slug')
+    {}
+    /**
+     * Returns the term language.
+     *
+     * @api
+     * @since 1.5.4
+     * @since 3.4 Accepts composite values for `$field`.
+     *
+     * @param int    $term_id Term ID.
+     * @param string $field Optional, the language field to return (@see PLL_Language), defaults to `'slug'`.
+     *                      Pass `\OBJECT` constant to get the language object. A composite value can be used for language
+     *                      term property values, in the form of `{language_taxonomy_name}:{property_name}` (see
+     *                      {@see PLL_Language::get_tax_prop()} for the possible values). Ex: `term_language:term_taxonomy_id`.
+     * @return string|int|bool|string[]|PLL_Language The requested field or object for the post language, `false` if no language is associated to that term.
+     */
+    function pll_get_term_language($term_id, $field = 'slug')
     {}
     /**
      * Returns an array of translations of a post.
@@ -37,14 +67,14 @@ namespace {
     function pll_get_post_translations($post_id)
     {}
     /**
-     * Allows to access the Polylang instance.
-     * However, it is always preferable to use API functions
-     * as internal methods may be changed without prior notice.
+     * Returns an array of translations of a term.
      *
+     * @api
      * @since 1.8
      *
-     * @return PLL_Frontend|PLL_Admin|PLL_Settings|PLL_REST_Request
+     * @param int $term_id Term ID.
+     * @return int[] An associative array of translations with language code as key and translation term ID as value.
      */
-    function PLL()
+    function pll_get_term_translations($term_id)
     {}
 }
