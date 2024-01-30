2c96d066bf4939e5e22f4e1d68afafabeabc557f
Paul Ryley <paul@geminilabs.io>
2024-01-29 22:58:49 -0500

    block all event function attributes in strings

--- plugin/Modules/Sanitizers/StringSanitizer.php
+++ plugin/Modules/Sanitizers/StringSanitizer.php
@@ -10,8 +10,8 @@ abstract class StringSanitizer extends AbstractSanitizer
 
     protected function kses(string $value): string
     {
-        $regex = '/on(beforeprint|beforeunload|blur|error|focus|hashchange|languagechange|load|message|offline|online|popstate|redo|resize|storage|undo|unload)\s*=/i';
-        $value = preg_replace($regex, '', $value); // remove all function attributes
+        $regex = '/on(abort|autocomplete|autocompleteerror|beforeprint|beforeunload|blur|cancel|canplay|canplaythrough|change|click|close|contextmenu|cuechange|dblclick|drag|dragend|dragenter|dragleave|dragover|dragstart|drop|durationchange|emptied|ended|error|focus|hashchange|input|invalid|keydown|keypress|keyup|languagechange|load|loadeddata|loadedmetadata|loadstart|message|mousedown|mouseenter|mouseleave|mousemove|mouseout|mouseover|mouseup|mousewheel|offline|online|pause|play|playing|popstate|progress|ratechange|redo|reset|resize|scroll|seeked|seeking|select|show|sort|stalled|storage|submit|suspend|timeupdate|toggle|undo|unload|volumechange|waiting)\s*=/i';
+        $value = preg_replace($regex, '', $value); // remove all event function attributes
         return $value;
     }
 
