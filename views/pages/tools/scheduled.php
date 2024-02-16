21b9e5d7541a8712059ba2db028f78efcd2247f8
Paul Ryley <paul@geminilabs.io>
2024-02-16 19:56:47 +0000

    update documentation

--- views/pages/tools/scheduled.php
+++ views/pages/tools/scheduled.php
@@ -2,7 +2,7 @@
 
 <div class="components-notice is-info" style="margin:20px 0 2px;">
     <p class="components-notice__content">
-        <?= _x('Site Reviews uses a versatile library called <a href="https://actionscheduler.org" target="_blank">Action Scheduler</a> to manage and schedule cron jobs in WordPress. Action Scheduler improves your site’s overall ability to process large tasks. Many other popular WordPress plugins like WooCommerce, RankMath, WPForms, and All in One SEO also rely on Action Scheduler to help execute actions more efficiently.', 'admin-text', 'site-reviews'); ?>
+        <?= _x('Site Reviews includes and uses the versatile <a href="https://actionscheduler.org" target="_blank">Action Scheduler</a> library to manage and schedule cron jobs in WordPress and improve your site’s overall ability to process large tasks. Many other popular WordPress plugins like WooCommerce, RankMath, WPForms, and All in One SEO also rely on Action Scheduler to execute actions more efficiently.', 'admin-text', 'site-reviews'); ?>
     </p>
 </div>
 
