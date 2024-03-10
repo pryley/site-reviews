b4b59406a0f93b9b3b8b0b3ee603880104d18cff
Paul Ryley <paul@geminilabs.io>
2024-03-10 00:19:52 +0000

    sanitize license keys

--- plugin/Application.php
+++ plugin/Application.php
@@ -195,6 +195,7 @@ final class Application extends Container
                     'class' => 'glsr-license-key regular-text',
                     'default' => '',
                     'label' => $name,
+                    'sanitizer' => 'text',
                     'tooltip' => sprintf(_x('Enter the license key here. Your license can be found on the %s page of your Nifty Plugins account.', 'License Keys (admin-text)', 'site-reviews'),
                         sprintf('<a href="https://niftyplugins.com/account/license-keys/" target="_blank">%s</a>', _x('License Keys', 'admin-text', 'site-reviews'))
                     ),
