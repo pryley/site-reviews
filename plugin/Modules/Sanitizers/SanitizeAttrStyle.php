ccaa032019d46673fb68fff0786b68cfb5a42f2b
Paul Ryley <paul@geminilabs.io>
2024-03-30 19:26:07 +0000

    fix attr-style sanitizer

--- plugin/Modules/Sanitizers/SanitizeAttrStyle.php
+++ plugin/Modules/Sanitizers/SanitizeAttrStyle.php
@@ -2,6 +2,8 @@
 
 namespace GeminiLabs\SiteReviews\Modules\Sanitizers;
 
+use GeminiLabs\SiteReviews\Helper;
+
 class SanitizeAttrStyle extends StringSanitizer
 {
     public function run(): string
@@ -10,18 +12,23 @@ class SanitizeAttrStyle extends StringSanitizer
         $parts = explode(';', $value);
         $parts = array_map(fn ($propVal) => $this->sanitizePropertyValue($propVal), $parts);
         $parts = array_filter($parts);
-        $value = implode(';', $parts);
+        $value = implode(' ', $parts);
+        $value = trim($value);
         return esc_attr($value);
     }
 
     protected function sanitizePropertyValue(string $propVal): string
     {
+        if (str_starts_with($propVal, 'http')) {
+            return '';
+        }
         $parts = explode(':', $propVal, 2);
         $property = preg_replace('/[^a-z\-]/', '', $parts[0] ?? '');
         $value = preg_replace('/[^\w,.:!#%"()\-\'\/ ]/', '', $parts[1] ?? '');
-        if (empty($property) || empty($value)) {
+        $value = trim($value);
+        if (empty($property) || Helper::isEmpty($value)) {
             return '';
         }
-        return "{$property}:{$value}";
+        return "{$property}: {$value};";
     }
 }
