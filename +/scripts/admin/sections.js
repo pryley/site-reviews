ed2035203efbf7ca6ec486f53667b65b82198e45
Paul Ryley <paul@geminilabs.io>
2024-03-31 14:34:24 +0100

    fix smartlinks in notices

--- +/scripts/admin/sections.js
+++ +/scripts/admin/sections.js
@@ -1,77 +1,65 @@
-/** global: GLSR, jQuery */
 
-const Sections = function (options) {
-    this.options = jQuery.extend({}, this.defaults, options);
-    this.tabs = document.querySelectorAll(this.options.tabSelector);
-    if (!this.tabs) return;
-    this.init_();
-    jQuery(() => this.scrollIntoView_(jQuery(localStorage.getItem('glsr-expand'))));
+const defaults = {
+    expandSelector: '.glsr-nav-view, .glsr-notice',
+    tabSelector: '.glsr-nav-tab',
 };
 
-Sections.prototype = {
-    defaults: {
-        expandSelectors: '.glsr-nav-view, .glsr-notice',
-        tabSelector: '.glsr-nav-tab',
-    },
+class Sections {
+    constructor (options) {
+        options = jQuery.extend({}, defaults, options);
+        jQuery(options.expandSelector).on('click', 'a[data-expand]', this.onExpand.bind(this))
+        jQuery(options.tabSelector).each((index, el) => {
+            jQuery(el).on('click', this.onClick.bind(this))
+        })
+        jQuery(document).on('wp-updates-notice-added', () => {
+            jQuery('.glsr-notice a[data-expand]').on('click', this.onExpand.bind(this))
+        })
+        this.scrollIntoView(jQuery(localStorage.getItem('glsr-expand')))
+    }
 
-    /** @return void */
-    init_: function () {
-        var self = this;
-        [].forEach.call(self.tabs, function (tab, index) {
-            tab.addEventListener('click', self.onClick_.bind(self));
-            tab.addEventListener('touchend', self.onClick_.bind(self));
-        }.bind(self));
-        jQuery(self.options.expandSelectors).on('click', 'a', function () {
-            var elId = this.dataset.expand;
-            localStorage.setItem('glsr-expand', elId);
-            self.scrollIntoView_(jQuery(elId));
-        });
-    },
+    onExpand (ev) {
+        const id = jQuery(ev.currentTarget).data('expand');
+        localStorage.setItem('glsr-expand', id)
+        this.scrollIntoView(jQuery(id))
+    }
 
-    /** @return void */
-    onClick_: function (ev) {
-        ev.preventDefault();
-        this.toggleCollapsibleViewSections_(ev.currentTarget);
-    },
+    onClick (ev) {
+        ev.preventDefault()
+        this.toggleTabView(jQuery(ev.currentTarget))
+    }
 
-    /** @return void */
-    scrollIntoView_: function (el) {
-        if (el.length) {
-            var navEl = el.closest('.glsr-nav-view')
-            var cardEl = el.closest('.glsr-card');
-            navEl.removeClass('collapsed');
-            this.toggleCollapsibleSections_(navEl);
-            navEl.removeClass('collapsed'); // just in case
-            cardEl.removeClass('closed').find('.glsr-accordion-trigger').attr('aria-expanded', true);
-            window.setTimeout(() => {
-                localStorage.removeItem('glsr-expand');
-                // if notices are visible, do not scroll section into view
-                if (jQuery('#glsr-notices .notice').length) {
-                    return;
-                }
-                // if height of card is greater than window height, scroll to start, otherwise scroll to center
-                cardEl[0].scrollIntoView({
-                    behavior: 'smooth',
-                    block: (cardEl.outerHeight() >= window.innerHeight ? 'start' : 'center'),
-                });
-            }, 10);
+    scrollIntoView ($el) {
+        if (!$el.length) return;
+        const $nav = $el.closest('.glsr-nav-view');
+        this.toggleSection($nav)
+        window.setTimeout(() => {
+            const $card = $el.closest('.glsr-card');
+            $card.removeClass('closed').find('.glsr-accordion-trigger').attr('aria-expanded', 'true')
+            localStorage.removeItem('glsr-expand')
+            // if notices are visible, do not scroll section into view
+            if (jQuery('#glsr-notices .notice').length) return;
+            // if height of card is greater than window height, scroll to start, otherwise scroll to center
+            $card[0].scrollIntoView({
+                behavior: 'smooth',
+                block: ($card.outerHeight() >= window.innerHeight ? 'start' : 'center'),
+            })
+        }, 10)
+    }
+
+    toggleSection ($el) {
+        const action = $el.hasClass('collapsed') ? 'removeClass' : 'addClass';
+        $el[action]('collapsed')
+            .find('.glsr-card')[action]('closed')
+            .find('.glsr-accordion-trigger').attr('aria-expanded', action === 'addClass' ? 'false' : 'true')
+    }
+
+    toggleTabView ($el) {
+        if (!$el.hasClass('nav-tab-active')) return;
+        const id = $el.data('id');
+        if (id) {
+            this.toggleSection(jQuery(`#${id}`))
         }
-    },
-
-    /** @return void */
-    toggleCollapsibleSections_: function (viewEl) {
-        var action = viewEl.hasClass('collapsed') ? 'remove' : 'add';
-        viewEl[action + 'Class']('collapsed')
-            .find('.glsr-card.postbox')[action + 'Class']('closed')
-            .find('.glsr-accordion-trigger').attr('aria-expanded', action !== 'add');
-    },
-
-    /** @return void */
-    toggleCollapsibleViewSections_: function (el) {
-        if (!el.classList.contains('nav-tab-active')) return;
-        var viewEl = jQuery('#' + el.dataset.id);
-        this.toggleCollapsibleSections_(viewEl);
-    },
-};
+    }
+}
 
 export default Sections;
