a755f474673d8aec6d4dc8c9a1a7eaed9bb41631
Paul Ryley <paul@geminilabs.io>
2024-05-13 11:31:31 +0100

    fix contact form 7 plugin style

--- views/styles/contact_form_7/field.php
+++ views/styles/contact_form_7/field.php
@@ -1,7 +1,8 @@
 <?php defined('ABSPATH') || exit; ?>
 
 <p class="{{ class }}" data-field="{{ field_name }}">
-    <label for="{{ for }}">{{ label_text }}<br>
+    <label for="<?php echo esc_attr($field->id); ?>">
+        <?php echo esc_html($field->label); ?><br>
         <span class="wpcf7-form-control-wrap">
             {{ field }}
             {{ errors }}
