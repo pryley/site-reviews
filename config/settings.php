ebcde890fee757b01e7f6fcb6c70c8252f203189
Paul Ryley <134939+pryley@users.noreply.github.com>
2024-06-07 15:56:58 +0100

    update schema integration setting options

--- config/settings.php
+++ config/settings.php
@@ -433,7 +433,7 @@ return [ // order is intentional
             'rankmath' => _x('RankMath Pro', 'plugin name (admin-text)', 'site-reviews'),
             'saswp' => _x('Schema & Structured Data for WP & AMP', 'plugin name (admin-text)', 'site-reviews'),
             'schema_pro' => _x('Schema Pro', 'plugin name (admin-text)', 'site-reviews'),
-            'seopress' => _x('SEOPress', 'plugin name (admin-text)', 'site-reviews'),
+            'seopress' => _x('SEOPress Pro', 'plugin name (admin-text)', 'site-reviews'),
         ],
         'sanitizer' => 'text',
         'tooltip' => _x('Integrate Site Reviews with a third-party schema plugin?', 'admin-text', 'site-reviews'),
