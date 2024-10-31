e27f4606704afa23ae46a0c2875484aaf4e1f5ea
Paul Ryley <134939+pryley@users.noreply.github.com>
2024-10-31 15:54:55 -0500

    fix range css

--- config/styles/divi.php
+++ config/styles/divi.php
@@ -4,10 +4,12 @@ return [
     'classes' => [
         'button' => 'et_pb_contact_submit et_pb_button',
         'field' => 'et_pb_contact_field',
-        'form' => 'et_pb_contact_form',
+        'fieldset' => 'et_pb_contact',
+        'form' => 'et_pb_contact_form clearfix',
         'input' => 'input',
         'input_checkbox' => 'input',
         'input_radio' => 'input',
+        'input_range' => 'input',
         'label' => 'et_pb_contact_form_label',
         'select' => 'et_pb_contact_select input',
         'textarea' => 'et_pb_contact_message input',
---
+++ views/styles/divi/type-range.php
@@ -0,0 +1,6 @@
+<?php defined('ABSPATH') || exit; ?>
+
+<span class="et_pb_contact_field_radio {{ class }}">
+    {{ input }}
+    <label for="{{ id }}">{{ text }}</label>
+</span>
