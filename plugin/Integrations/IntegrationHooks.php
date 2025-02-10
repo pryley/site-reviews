370976d0ce613e8eaced790d784e1d58606d78c0
Paul Ryley <134939+pryley@users.noreply.github.com>
2025-02-10 12:24:12 +0000

    fix integration version check

--- plugin/Integrations/IntegrationHooks.php
+++ plugin/Integrations/IntegrationHooks.php
@@ -4,7 +4,6 @@ namespace GeminiLabs\SiteReviews\Integrations;
 
 use GeminiLabs\SiteReviews\Hooks\AbstractHooks;
 use GeminiLabs\SiteReviews\Modules\Notice;
-use GeminiLabs\SiteReviews\Modules\Sanitizer;
 
 abstract class IntegrationHooks extends AbstractHooks
 {
@@ -20,19 +19,19 @@ abstract class IntegrationHooks extends AbstractHooks
 
     protected function isVersionSupported(): bool
     {
-        $version = glsr(Sanitizer::class)->sanitizeVersion($this->version());
-        $supportedVersion = glsr(Sanitizer::class)->sanitizeVersion($this->supportedVersion());
+        $supportedVersion = sanitize_text_field($this->supportedVersion());
         if (empty($supportedVersion)) {
             return true;
         }
+        $version = sanitize_text_field($this->version());
         return version_compare($version, $supportedVersion, '>=');
     }
 
     protected function notify(string $name): void
     {
-        $notice = _x('Update %s to v%s or higher to enable the integration with Site Reviews.', 'admin-text', 'site-reviews');
-        $version = glsr(Sanitizer::class)->sanitizeVersion($this->supportedVersion());
-        glsr(Notice::class)->addWarning(sprintf($notice, $name, $version));
+        $notice = _x('Update %s to version %s or higher to enable the integration with Site Reviews.', 'admin-text', 'site-reviews');
+        $supportedVersion = sanitize_text_field($this->supportedVersion());
+        glsr(Notice::class)->addWarning(sprintf($notice, $name, $supportedVersion));
     }
 
     protected function supportedVersion(): string
