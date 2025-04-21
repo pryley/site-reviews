289f4beae7225842c3ddf7925eb54256008e562a
Paul Ryley <134939+pryley@users.noreply.github.com>
2025-04-21 02:15:47 +0200

    use --experimental-modules

--- +/blocks/webpack.config.js
+++ +/blocks/webpack.config.js
@@ -1,30 +1,42 @@
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 const { resolve } = require('path');
 
-module.exports = {
-  ...defaultConfig,
-  output: {
-    ...defaultConfig.output,
-    path: resolve(process.cwd(), 'assets/blocks'),
-  },
-  module: {
-    ...defaultConfig.module,
-    rules: [
-      ...defaultConfig.module.rules,
-      {
-        test: /(j|t)sx?$/,
-        include: [
-          resolve(process.cwd(), '+/packages'),
-        ],
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@wordpress/babel-preset-default',
-            ],
+let config = defaultConfig;
+
+// When --experimental-modules is passed, config is an array (with two objects)
+// instead of an object. This ensures that config is always an array.
+if (!Array.isArray(config)) {
+  config = [ config ];
+}
+
+config = config.map(conf => {
+  return {
+    ...conf,
+    output: {
+      ...conf.output,
+      path: resolve(process.cwd(), 'assets/blocks'),
+    },
+    module: {
+      ...conf.module,
+      rules: [
+        ...conf.module.rules,
+        {
+          test: /(j|t)sx?$/,
+          include: [
+            resolve(process.cwd(), '+/packages'),
+          ],
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@wordpress/babel-preset-default',
+              ],
+            },
           },
         },
-      },
-    ],
-  },
-};
+      ],
+    },
+  }
+});
+
+module.exports = config;
