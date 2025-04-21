0b6c67e098516681f885b32f02e5727b5cc0eb80
Paul Ryley <134939+pryley@users.noreply.github.com>
2025-04-21 02:17:23 +0200

    add summary block styles

--- plugin/Integrations/Gutenberg/Blocks/SiteReviewsSummaryBlock.php
+++ plugin/Integrations/Gutenberg/Blocks/SiteReviewsSummaryBlock.php
@@ -7,6 +7,20 @@ use GeminiLabs\SiteReviews\Shortcodes\SiteReviewsSummaryShortcode;
 
 class SiteReviewsSummaryBlock extends Block
 {
+    public function register(): void
+    {
+        parent::register();
+        register_block_style('site-reviews/summary', [
+            'name' => 'variant-1',
+            'label' => _x('Style 1', 'admin-text', 'site-reviews'),
+            'is_default' => true,
+        ]);
+        register_block_style('site-reviews/summary', [
+            'name' => 'variant-2',
+            'label' => _x('Style 2', 'admin-text', 'site-reviews'),
+        ]);
+    }
+
     public function shortcode(): ShortcodeContract
     {
         return glsr(SiteReviewsSummaryShortcode::class);
