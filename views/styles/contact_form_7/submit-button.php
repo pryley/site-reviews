3041b8dfa42e61f2e9018ea5edfbc08318df1a53
Paul Ryley <134939+pryley@users.noreply.github.com>
2025-04-23 13:50:55 +0200

    fix contact form 7 button style

--- views/styles/contact_form_7/submit-button.php
+++ views/styles/contact_form_7/submit-button.php
@@ -1,5 +1,9 @@
 <?php defined('ABSPATH') || exit; ?>
 
 <p data-field="submit-button">
-    <button type="submit" class="{{ class }}" aria-busy="false" data-loading="{{ loading_text }}">{{ text }}</button>
+    <span class="wp-block-buttons">
+        <span class="wp-block-button">
+            <button type="submit" class="{{ class }}" aria-busy="false" data-loading="{{ loading_text }}">{{ text }}</button>
+        </span>
+    </span>
 </p>
