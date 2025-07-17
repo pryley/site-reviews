e72e1325502c8d931e16f92b1cc97cb4291e895f
Paul Ryley <134939+pryley@users.noreply.github.com>
2025-07-17 15:07:13 -0500

    update breakdance stub

--- tests/phpstan/stubs/breakdance.php
+++ tests/phpstan/stubs/breakdance.php
@@ -1,5 +1,25 @@
 <?php
 
+namespace Breakdance
+{
+    trait Singleton
+    {
+        /**
+         * @return self
+         */
+        final public static function getInstance(): self
+        {
+        }
+        // Prevent cloning of the instance
+        public function __clone()
+        {
+        }
+        // Prevent deserialization of the instance
+        public function __wakeup()
+        {
+        }
+    }
+}
 namespace Breakdance\Lib\Vendor\League\HTMLToMarkdown
 {
     interface ElementInterface
@@ -38,6 +58,211 @@ namespace Breakdance\Elements
 {
     class Element implements \Breakdance\Lib\Vendor\League\HTMLToMarkdown\ElementInterface
     {
+        /**
+         * @return string
+         */
+        public static function defaultTag()
+        {
+        }
+        /**
+         * @return string
+         */
+        public static function uiIcon()
+        {
+        }
+        /**
+         * @return string[]
+         */
+        public static function tagOptions()
+        {
+        }
+        /**
+         * @return string|false
+         */
+        public static function tagControlPath()
+        {
+        }
+        /**
+         * @return string
+         */
+        public static function template()
+        {
+        }
+        /**
+         * @return string
+         */
+        public static function slug()
+        {
+        }
+        /**
+         * @return string|false
+         */
+        public static function name()
+        {
+        }
+        /**
+         * @return string|false
+         */
+        public static function className()
+        {
+        }
+        /**
+         * @return string
+         */
+        public static function category()
+        {
+        }
+        /**
+         * @return mixed|false
+         */
+        public static function badge()
+        {
+        }
+        /**
+         * @return Control[]
+         */
+        public static function contentControls()
+        {
+        }
+        /**
+         * @return Control[]
+         */
+        public static function designControls()
+        {
+        }
+        /**
+         * @return Control[]
+         */
+        public static function settingsControls()
+        {
+        }
+        /**
+         * @return DynamicPropertyPath[]|false|null
+         */
+        public static function dynamicPropertyPaths()
+        {
+        }
+        /**
+         * @return mixed
+         */
+        public static function defaultProperties()
+        {
+        }
+        /**
+         * @return false|string[]
+         */
+        public static function defaultChildren()
+        {
+        }
+        /**
+         * @return ElementAttribute[]|false
+         */
+        public static function attributes()
+        {
+        }
+        /**
+         * @return string
+         */
+        public static function defaultCSS()
+        {
+        }
+        /**
+         * @return string
+         */
+        public static function cssTemplate()
+        {
+        }
+        /**
+         * @return array{scripts?:string[],inlineScripts?:string[],styles?:string[],inlineStyles?:string[],builderCondition?:string,frontendCondition?:string}[]|false
+         */
+        public static function dependencies()
+        {
+        }
+        /**
+         * @return ElementSettings|false
+         */
+        public static function settings()
+        {
+        }
+        /**
+         * @return BuilderActions|false
+         */
+        public static function actions()
+        {
+        }
+        /**
+         * @return array{location:string,cssProperty:string,affectedPropertyPath:string}[]|false
+         */
+        public static function spacingBars()
+        {
+        }
+        /**
+         * @return mixed
+         */
+        public static function nestingRule()
+        {
+        }
+        /**
+         * @return string
+         */
+        public static function tag()
+        {
+        }
+        /**
+         * @param mixed $propertiesData
+         * @param mixed $parentPropertiesData
+         * @param bool $isBuilder
+         * @param int $repeaterItemNodeId
+         * @return string
+         */
+        public static function ssr($propertiesData, $parentPropertiesData = [], $isBuilder = false, $repeaterItemNodeId = null)
+        {
+        }
+        /**
+         * @return false|mixed
+         */
+        public static function addPanelRules()
+        {
+        }
+        public static function requiredPlugins()
+        {
+        }
+        /**
+         * @return boolean
+         */
+        public static function experimental()
+        {
+        }
+        /**
+         * @return int
+         */
+        public static function order()
+        {
+        }
+        /**
+         * @return array{name:string,template:string}[]|false
+         */
+        public static function additionalClasses()
+        {
+        }
+        /**
+         * @return mixed|false
+         */
+        public static function projectManagement()
+        {
+        }
+        /**
+         * @return string[]|false
+         */
+        public static function propertyPathsToWhitelistInFlatProps()
+        {
+        }
+        /**
+         * @return string[]|false
+         */
+        public static function propertyPathsToSsrElementWhenValueChanges()
+        {
+        }
     }
     /**
      * @param string $slug
@@ -74,6 +299,25 @@ namespace Breakdance\Elements\PresetSections
     function getPresetSection($presetSlug, $label = false, $sectionSlug = false, $options = false)
     {
     }
+    class PresetSectionsController
+    {
+        use \Breakdance\Singleton;
+        /**
+         * @param string $slug
+         * @param Control $section
+         * @param boolean $availableInElementStudio
+         * @param array{relativePropertyPathsToWhitelistInFlatProps?: string[], relativeDynamicPropertyPaths?: DynamicPropertyPath[], codeHelp?: string}|null $options
+         */
+        public function register($slug, $section, $availableInElementStudio = false, $options = null)
+        {
+        }
+        /**
+         * @return Preset[]
+         */
+        public function getAvailableInElementStudio()
+        {
+        }
+    }
 }
 namespace Breakdance\ElementStudio
 {
@@ -99,6 +343,12 @@ namespace Breakdance\Permissions
      * @throws \Exception
      */
     function hasMinimumPermission($minimumPermission, $userId = 0)
+    {
+    }
+}
+namespace EssentialElements
+{
+    class Formdesignoptions extends \Breakdance\Elements\Element
     {
     }
 }
