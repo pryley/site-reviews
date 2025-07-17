2e82e0c28dd0608c54f760b5f0892026f036ce66
Paul Ryley <134939+pryley@users.noreply.github.com>
2025-07-17 14:51:17 -0500

    use glsr_premium_link function

--- views/pages/documentation/shortcodes/site_review/theme.php
+++ views/pages/documentation/shortcodes/site_review/theme.php
@@ -1,6 +1,6 @@
 <p class="glsr-heading">theme</p>
 <div class="glsr-notice-inline components-notice is-warning">
-    <p class="components-notice__content">The <a href="<?php echo glsr_admin_url('addons'); ?>">Review Themes</a> addon is required to use this shortcode option.</p>
+    <p class="components-notice__content">The <?php echo glsr_premium_link('site-reviews-themes'); ?> addon is required to use this shortcode option.</p>
 </div>
 <p>Include the "theme" option to display the review using a custom Review Theme. Accepted value is the Post ID of the custom Review Theme.</p>
 <p><span class="required">Important:</span> The custom Review Theme will override the hide option used on this shortcode.</p>
--- views/pages/documentation/shortcodes/site_reviews_summary/theme.php
+++ views/pages/documentation/shortcodes/site_reviews_summary/theme.php
@@ -1,6 +1,6 @@
 <p class="glsr-heading">theme</p>
 <div class="glsr-notice-inline components-notice is-warning">
-    <p class="components-notice__content">The <a href="<?php echo glsr_admin_url('addons'); ?>">Review Themes</a> addon is required to use this shortcode option.</p>
+    <p class="components-notice__content"><?php echo glsr_premium_link('site-reviews-themes'); ?> addon is required to use this shortcode option.</p>
 </div>
 <p>Include the "theme" option to use the rating style and rating colors of a custom Review Theme. Accepted value is the Post ID of the custom Review Theme.</p>
 <div class="shortcode-example">
