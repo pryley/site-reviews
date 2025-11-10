f3f286a448032bd2587d475c9bfd6463d373b5d0
Paul Ryley <134939+pryley@users.noreply.github.com>
2025-11-10 22:33:14 +0100

    add data-type attribute to fields

--- templates/form/field.php
+++ templates/form/field.php
@@ -3,7 +3,7 @@
  * @version 1.0.0
  */
 ?>
-<div class="{{ class }}" data-field="{{ field_name }}">
+<div class="{{ class }}" data-field="{{ field_name }}" data-type="{{ field_type }}">
     {{ label }}
     {{ field }}
     {{ errors }}
