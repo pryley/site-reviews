1b7028e79a38e6011d0cbe3c6f45bbd37dd2ed99
Paul Ryley <134939+pryley@users.noreply.github.com>
2025-11-10 22:33:25 +0100

    use subgroups in toggle field

---
+++ templates/form/field_toggle.php
@@ -0,0 +1,12 @@
+<?php defined('ABSPATH') || exit;
+/**
+ * @version 1.0.0
+ */
+?>
+<div class="{{ class }}" data-field="{{ field_name }}" data-type="{{ field_type }}">
+    {{ label }}
+    <div class="glsr-field-subgroup">
+        {{ field }}
+    </div>
+    {{ errors }}
+</div>
