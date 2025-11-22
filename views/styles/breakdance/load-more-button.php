3059cd427c2f77e9bd434e86488a70a77ec89c31
Paul Ryley <134939+pryley@users.noreply.github.com>
2025-11-22 10:56:49 +0100

    fix breakdance loadmore button

---
+++ views/styles/breakdance/load-more-button.php
@@ -0,0 +1,8 @@
+<?php defined('ABSPATH') || exit; ?>
+
+<button type="button" class="{{ class }}" 
+    aria-label="{{ screen_reader_text }}"
+    aria-busy="false" 
+    data-loading="{{ loading_text }}" 
+    data-page="{{ page }}"
+>{{ text }}</button>
