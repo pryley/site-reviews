71cea351638c4aa5808a6c2356d7385db8157b89
Paul Ryley <134939+pryley@users.noreply.github.com>
2025-11-24 00:41:20 +0100

    build and misc

--- plugin/Contracts/FieldContract.php
+++ plugin/Contracts/FieldContract.php
@@ -6,31 +6,31 @@ namespace GeminiLabs\SiteReviews\Contracts;
  * This class normalizes the DOM markup surrounding the field element.
  * Styled class attributes for the field are merged here.
  *
- * @property string       $after
- * @property bool         $checked
- * @property string       $class
- * @property string       $description
- * @property bool         $disabled
- * @property array        $errors
- * @property string       $group
- * @property string       $id
- * @property bool         $is_custom
- * @property bool         $is_raw
- * @property bool         $is_valid
- * @property string       $label
- * @property array        $labels
- * @property bool         $multiple
- * @property string       $name
- * @property array        $options
- * @property string       $original_name
- * @property string       $original_type
- * @property bool         $required
- * @property bool         $selected
- * @property int          $tabindex
- * @property string       $tag
- * @property string       $text
- * @property string       $type
- * @property string|array $value
+ * @property string        $after
+ * @property bool          $checked
+ * @property string        $class
+ * @property string        $description
+ * @property bool          $disabled
+ * @property array         $errors
+ * @property string        $group
+ * @property string        $id
+ * @property bool          $is_custom      If the field in a custom form
+ * @property bool          $is_raw
+ * @property bool          $is_valid
+ * @property string        $label
+ * @property array         $labels
+ * @property bool          $multiple
+ * @property string        $name
+ * @property array         $options
+ * @property string        $original_name
+ * @property string        $original_type
+ * @property bool          $required
+ * @property bool          $selected
+ * @property int           $tabindex
+ * @property string        $tag
+ * @property string        $text
+ * @property string        $type
+ * @property string|array  $value
  */
 interface FieldContract
 {
