786f5813a9f25565e8fbe5450e5f0ae35475b1f0
Paul Ryley <134939+pryley@users.noreply.github.com>
2026-02-24 13:06:53 -0400

    update shortcode docs

--- views/pages/documentation/shortcodes/site_reviews_summary/hide.php
+++ views/pages/documentation/shortcodes/site_reviews_summary/hide.php
@@ -1,7 +1,7 @@
 <?php defined('ABSPATH') || exit; ?>
 
 <p class="glsr-heading">hide</p>
-<p>By default the shortcode displays all fields. Use the "hide" option to hide any specific fields you don't want to show. Include the "if_empty" value to hide the shortcode when there are no reviews to summarise. If all fields are hidden, the shortcode will not be displayed.</p>
+<p>By default the shortcode displays all fields. Use the "hide" option to hide any specific fields you don't want to show. Include "if_empty" to hide the shortcode when the value is 0. If all fields are hidden, the shortcode will not be displayed.</p>
 <div class="shortcode-example">
     <pre><code class="language-shortcode">[site_reviews_summary hide="bars,if_empty,rating,stars,summary"]</code></pre>
 </div>
