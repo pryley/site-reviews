11737e4809f951629e5eefa1c06f34f1129421d2
Paul Ryley <134939+pryley@users.noreply.github.com>
2026-04-11 22:02:39 -0400

    fix mce-plugin with new build tooling

--- +/scripts/mce-plugin.js
+++ +/scripts/mce-plugin.js
@@ -1,7 +1,7 @@
 /** global: GLSR */
 (tinymce => {
     'use strict';
-    tinymce.PluginManager.add('glsr_shortcode', editor => {
+    tinymce.PluginManager.add('glsr_shortcode', function (editor) {
         editor.addCommand('GLSR_Shortcode', () => {
             GLSR.shortcode.create(editor.id);
         });
--- assets/scripts/mce-plugin.js
+++ assets/scripts/mce-plugin.js
@@ -1,1 +1,1 @@
-!function(){"use strict";window.tinymce.PluginManager.add("glsr_shortcode",d=>{d.addCommand("GLSR_Shortcode",()=>{GLSR.shortcode.create(d.id)})})}();
+!function(){"use strict";window.tinymce.PluginManager.add("glsr_shortcode",function(d){d.addCommand("GLSR_Shortcode",()=>{GLSR.shortcode.create(d.id)})})}();
