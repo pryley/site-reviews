79fbbbe13773c80f845587367ee948d86b8d85bb
Paul Ryley <134939+pryley@users.noreply.github.com>
2026-09-04 16:36:47 +0200

    v8.3.0 release

--- site-reviews.php
+++ site-reviews.php
@@ -7,7 +7,7 @@
  * Plugin Name:          Site Reviews
  * Plugin URI:           https://wordpress.org/plugins/site-reviews
  * Description:          Receive and display reviews on your website
- * Version:              8.2.2
+ * Version:              8.3.0
  * Author:               Paul Ryley
  * Author URI:           https://site-reviews.com
  * License:              GPL3
@@ -17,7 +17,7 @@
  * Text Domain:          site-reviews
  * Domain Path:          languages
  * WC requires at least: 9.6
- * WC tested up to:      11.0
+ * WC tested up to:      11.1
  */
 defined('ABSPATH') || exit;
 
